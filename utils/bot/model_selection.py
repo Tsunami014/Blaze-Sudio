e0c7a14c2fbaa9b2e24d71517e0079d31e774eb9
Max Worrall <max.worrall@education.nsw.gov.au>
2023-08-24 19:38:45 +1000

    Added uninstall button, uninstall confirmation and info screen with text wrap dang chatGPT's good

--- utils/bot/model_selection.py
+++ utils/bot/model_selection.py
@@ -1,6 +1,5 @@
 # AI generated file
-
-import pygame
+import pygame, textwrap
 
 # Initialize Pygame
 pygame.init()
@@ -16,6 +15,7 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 gray = (200, 200, 200)
 green = (0, 255, 0)
+red = (255, 0, 0)
 
 # Fonts
 font = pygame.font.Font(None, 36)
@@ -71,8 +71,57 @@ while running:
         if model["downloaded"]:
             pygame.draw.rect(screen, green, (model_rect.left, model_rect.top, 5, model_rect.height))
             
-        text = small_font.render(model["name"], True, white)  # Change font color to white
+        text = small_font.render(model["name"], True, white)
         screen.blit(text, (model_list.left + 10, model_list.top + i * 50 + 10 - scroll_offset))
+
+    # Draw uninstall button
+    if selected_model is not None and models[selected_model]["downloaded"]:
+        uninstall_button = pygame.Rect(400, 560, 200, 30)
+        pygame.draw.rect(screen, red, uninstall_button, border_radius=8)
+        text = small_font.render("Uninstall", True, white)
+        screen.blit(text, (uninstall_button.centerx - 45, uninstall_button.centery - 10))
+        
+        # Handle uninstall action
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if uninstall_button.collidepoint(event.pos):
+                pygame.draw.rect(screen, white, (400, 300, 350, 150), border_radius=10)  # Clear the area
+                pygame.draw.rect(screen, gray, (400, 300, 350, 150), border_radius=10)  # Dialog background
+                confirmation_text = "Are you sure you want to uninstall?"
+                confirm_text = small_font.render(confirmation_text, True, black)
+                screen.blit(confirm_text, (420, 320))
+                yes_button = pygame.Rect(420, 380, 80, 30)
+                no_button = pygame.Rect(530, 380, 80, 30)
+                pygame.draw.rect(screen, red, yes_button, border_radius=8)
+                pygame.draw.rect(screen, green, no_button, border_radius=8)
+                yes_text = small_font.render("Yes", True, white)
+                no_text = small_font.render("No", True, white)
+                screen.blit(yes_text, (yes_button.centerx - 15, yes_button.centery - 10))
+                screen.blit(no_text, (no_button.centerx - 15, no_button.centery - 10))
+                pygame.display.flip()
+
+                # Wait for user input
+                confirm_waiting = True
+                while confirm_waiting:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            running = False
+                            confirm_waiting = False
+                        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                            if yes_button.collidepoint(event.pos):
+                                models[selected_model]["downloaded"] = False
+                                confirm_waiting = False
+                            elif no_button.collidepoint(event.pos):
+                                confirm_waiting = False
+
+    # Draw info section
+    info_section = pygame.Rect(400, 100, 350, 400)
+    pygame.draw.rect(screen, gray, info_section, border_radius=10)
+    if selected_model is not None:
+        selected_info = models[selected_model]["description"]
+        info_lines = textwrap.wrap(selected_info, width=40)  # Wrap the text
+        for i, line in enumerate(info_lines):
+            text = small_font.render(line, True, black)
+            screen.blit(text, (info_section.left + 10, info_section.top + i * 30 + 10))
 
     # Draw scroll bar
     pygame.draw.rect(screen, black, scroll_bar, border_radius=10)
