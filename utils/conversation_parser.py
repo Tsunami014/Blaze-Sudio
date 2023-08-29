1f4872eb47d7e52521f18a751237e4c00480c695
Max Worrall <max.worralL@education.nsw.gov.au>
2023-08-29 16:33:39 +1000

    Added Summary and Set Level classes! Things are working!!

--- utils/conversation_parser.py
+++ utils/conversation_parser.py
@@ -1,3 +1,49 @@
+from nltk import word_tokenize as wt
+from nltk.tokenize.treebank import TreebankWordDetokenizer as TWD
+
+def SL(txt, lvl=1): # Set Level
+    amnt = ''.join(['`' for _ in range(lvl)])
+    return amnt + txt + amnt
+
+class Summary:
+    def __init__(self, description):
+        self.txt = self._parse(description)
+    
+    def _parse(self, description):
+        end = []
+        temp = []
+        lvl = 0
+        prev = 0 # 0 = nothing was before, 1 = going up, 2 = going down
+        l = []
+        for i in wt(description): l.append(i) if i != '``' else l.extend(['`', '`'])
+        for wrd in l:
+            if wrd == '`':
+                if prev == 0:
+                    if temp != []:
+                        end.append({'txt': TWD().detokenize(temp), 'lvl': lvl})
+                        temp = []
+                    if lvl != 0:
+                        lvl -= 1
+                        if lvl != 0:
+                            prev = 2
+                    else:
+                        lvl += 1
+                        prev = 1
+                elif prev == 1:
+                    lvl += 1
+                else: # prev == 2
+                    lvl -= 1
+                    if lvl == 0:
+                        prev = 0
+            else:
+                temp.append(wrd)
+                prev = 0
+        if temp != []:
+            end.append({'txt': TWD().detokenize(temp), 'lvl': lvl})
+        return end
+
+# print(Summary('`Hello!```Bye.``Hi again!').txt)
+# print(Summary('%s%s - noo! %s' % (SL('Hello!', 2), SL('Wait...'), SL('I forgot!!', 987))).txt)
 
 def PARSE(cnvrs, summary_level, prompt_type):
     """
