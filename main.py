18dfd4edfe38267b9a50b2f3f2dfd191c11721c2
Max Worrall <max.worralL@education.nsw.gov.au>
2023-08-31 08:11:16 +1000

    Edited main.py to make it work with the new inputbox! removed other AI... for now.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from utils import Character
 from utils.AIs import *
+from graphics.GUI import InputBox, RESIZE_H
 
 class GameEngine: #TODO: Better name
     def __init__(self):
-        bot = ChatGPTBot()
-        userbot = UserBot() # TODO: make a userbot, which inputs the user's input and detects whether they interrupt or not, etc. and whatevs
         self.characters = [
-            Character(bot, 'AI', 'An AI assistant for the user.'),
-            Character(userbot, 'User', '')
+            Character(AI(), 'AI', 'An AI assistant for the user.'),
+            Character(UserBot(), 'User', '')
         ]
         self.ongoing = []
     
@@ -43,13 +42,19 @@ class GameEngine: #TODO: Better name
                     interrupts[j] = j.should_interrupt(said, who) # change params for multi-conversation/people support
 
 if __name__ == '__main__':
-    GE = GameEngine()
     import pygame
     pygame.init()
 
-    WIN = pygame.display.set_mode()
-    sze = WIN.get_size()
+    WIN = pygame.display.set_mode((800, 500))
     pygame.display.set_caption('AIHub')
+
+    GE = GameEngine()
+    input_box = InputBox(100, 100, 140, 32, '', resize=RESIZE_H, maxim=1000)
+    def _(cnvrs):
+        input_box.rect.move_ip(0-input_box.rect.topleft[0], 0-input_box.rect.topleft[1])
+        input_box.rect.move_ip(*pygame.mouse.get_pos())
+        return {'choices': [{'message': {'role': 'user', 'content': input_box.interrupt(WIN)}}]}
+    GE.characters[1].AI._call_ai = _
 
     txt = pygame.font.SysFont('Arial', 20)
 
@@ -63,10 +68,10 @@ if __name__ == '__main__':
                 if event.key == pygame.K_ESCAPE:
                     run = False
                 if event.key == pygame.K_SPACE:
-                    GE.call(GE.characters[1], GE.characters[0])
+                    GE.call(GE.characters[1], [])#GE.characters[0])
         
         WIN.fill((0, 0, 0))
-        for who, said in range(len(GE.ongoing)):
+        for who, said in GE.ongoing:
             WIN.blit(txt.render(said, True, (255, 255, 255)), (0, 0))
         pygame.display.update()
         clock.tick(60)
