a0a8ba80db6980f9ba1546d963c5ecf46dd2b1d6
Max Worrall <max.worrall@education.nsw.gov.au>
2023-09-18 16:02:55 +1000

    added to this

--- utils/conversion_parse/__init__.py
+++ utils/conversion_parse/__init__.py
@@ -1,4 +1,6 @@
 try:
     from utils.conversion_parse.conversation_parser import *
+    from utils.conversion_parse.consts import *
 except ImportError:
     from conversion_parse.conversation_parser import *
+    from conversion_parse.consts import *
