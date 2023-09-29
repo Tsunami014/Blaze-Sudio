7aef526099ba3461b049e230b2f4ebe0cc852c62
Max Worrall <max.worrall@education.nsw.gov.au>
2023-09-29 18:05:24 +1000

    feat(small): 🚀 Integrated the tinyllm with AIs.py :)

--- utils/bot/tinyllm.py
+++ utils/bot/tinyllm.py
@@ -8,27 +8,17 @@ except:
         from basebots import BaseBot
 
 class tinyllm(BaseBot):
+    def __init__(self, ram='base'):
+        """
+        A tinyllm bot. This runs offline!
+        Parameters
+        ----------
+        ram : str
+            The amount of RAM to use. Can be 'base' or '4gb' or '15M'. Nothing really matters, as long as it works. Defaults to 'base'.
+        """
+        self.ram = ram
+        super().__init__()
     
     async def _call_ai(self, cnvrs):
         out = 'hello!'#str(cnvrs)
         return out
-    
-    async def should_interrupt(self, conv, description=''):
-        """
-        Parameters
-        ----------
-        conv : str
-            The conversation so far
-        description : str, optional
-            The description of the conversation, by default ''
-        
-        Returns
-        -------
-        str
-            The interrupt code
-        """
-        conv = PARSE([(3, 0), 2], description+\
-                     Summary('*\n*`You are to `(```make a statement about```|*reply to*)* this conversation*``;``*\n**Respond with one character from the following list:\n**a=agree;i=interrupt;l=leave;s=*(``say something``|*speak*)').get(0)\
-                     , conv, 'Bot') #TODO: change params
-        await self(conv)
-        return self.out
