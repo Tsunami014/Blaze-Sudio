e2a1ef7d9d8ff33f1c3d2450ecc91daabf4cd2f2
Max Worrall <max.worrall@education.nsw.gov.au>
2023-09-29 16:25:46 +1000

    chore(small): 🏗️ Changed the basebots to be in a separate file than the AIs.py
    Now we can use it without recuring imports!

--- utils/bot/__init__.py
+++ utils/bot/__init__.py
@@ -2,7 +2,9 @@ try:
     from utils.bot.gpt4real import *
     from utils.bot.set_preferences import *
     from utils.bot.AIs import *
+    from utils.bot.basebots import *
 except ImportError:
     from bot.gpt4real import *
     from bot.set_preferences import *
     from bot.AIs import *
+    from bot.basebots import *
