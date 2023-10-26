3f25997132e23817cc2b8ab7864a52499affe365
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-10-27 08:16:37 +1100

    feat(medium): 🚀 Copied over some randomGuiElements from some other repos I've made
    And added them to __init__.py for graphics too

--- graphics/GUI/__init__.py
+++ graphics/GUI/__init__.py
@@ -2,7 +2,9 @@ try:
     from graphics.GUI.inputbox import RESIZE_W, RESIZE_H, RESIZE_NONE, InputBox, renderTextCenteredAt
     from graphics.GUI.pyguix import *
     from graphics.GUI.textboxify import *
+    from graphics.GUI.randomGUIelements import *
 except ImportError:
     from inputbox import RESIZE_W, RESIZE_H, RESIZE_NONE, InputBox, renderTextCenteredAt
     from pyguix import *
     from textboxify import *
+    from randomGUIelements import *
