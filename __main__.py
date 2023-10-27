71dc7d2962962ded3893abaf47b7c051ff606e82
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-10-27 15:57:06 +1100

    feat(world): ✨ Added world selection to main and also added info for the generated ldtk files. Also added a folder for the generated files :)

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 # TODO: make each file not need to be dependant on python files from above folders
-import pygame
+import pygame, os, json
 from graphics.GUI import Button
 pygame.init()
 
@@ -16,6 +16,8 @@ BLACK = (0, 0, 0)
 font = pygame.font.SysFont('', 52)
 title = pygame.font.SysFont('Comic Sans MS', 64, True)
 codefont = pygame.font.SysFont('Lucida Sans Typewriter', 16)
+
+btngen = lambda txt, col, txtcol=BLACK: Button(WIN, txt, col, txtcol, font=font, max_width=300)
 
 class TerminalBar:
     def __init__(self, win, font, spacing=5):
@@ -56,9 +58,41 @@ class Game:
     def __init__(self):
         self.clock = pygame.time.Clock()
         self.TB = TerminalBar(WIN, codefont)
+    def world_select(self):
+        welcome = title.render('World selection', 2, BLACK)
+        worlds = [i for i in os.scandir('data/worlds') if i.is_file() and i.name.endswith('.ldtk')]
+        worldinfo = [json.load(open('data/worlds/'+i.name))['AIHub info'] for i in worlds]
+        btns = [btngen('Back', (125, 125, 125))]
+        btns.extend([btngen(i[1], BLUE) for i in worldinfo])
+        blank = font.render(' ', 2, BLACK)
+        t = blank
+        while True:
+            WIN.fill(WHITE)
+            updates = [btns[i].update(0, welcome.get_height() + t.get_height() + 20 + sum([btns[j].nsurface.get_height() + 20 for j in range(i)])) for i in range(len(btns))]
+            if any(updates) and not updates[0]: t = font.render(worldinfo[updates[1:].index(True)][2], 2, BLACK)
+            else: t = blank
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        return
+                    elif self.TB.active != -1:
+                        self.TB.pressed(event)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == pygame.BUTTON_LEFT:
+                        for i in [btns[j] for j in range(len(btns)) if updates[j]]:
+                            if i == btns[0]: # back
+                                return None
+                            else: print(i)
+                        self.TB.toggleactive(not self.TB.collides(*event.pos))
+            WIN.blit(welcome, (WIN.get_width()/2-welcome.get_width()/2, 0))
+            WIN.blit(t, (WIN.get_width()/2-t.get_width()/2, welcome.get_height()+10))
+            self.TB.update()
+            pygame.display.flip()
+            self.clock.tick(60)
     def welcome(self):
         welcome = title.render('Welcome to AIHub! :)', 2, BLACK)
-        btngen = lambda txt, col, txtcol=BLACK: Button(WIN, txt, col, txtcol, font=font, max_width=300)
         btns = [btngen('Start', GREEN), btngen('Tutorial', RED)]
         run = True
         while run:
@@ -77,7 +111,9 @@ class Game:
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == pygame.BUTTON_LEFT:
                         for i in [btns[j] for j in range(len(btns)) if updates[j]]:
-                            print(i)
+                            if i == btns[0]: # start
+                                g.world_select()
+                            else: print(i)
                         self.TB.toggleactive(not self.TB.collides(*event.pos))
             WIN.blit(welcome, (WIN.get_width()/2-welcome.get_width()/2, WIN.get_height()/2-welcome.get_height()/2))
             self.TB.update()
