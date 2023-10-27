8961c0bcb9b90da75cb41723213b66493dd80bf2
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-10-27 15:28:39 +1100

    refactor(main): ♻️ Made the main file into a class (tho good luck using it outside the main file itself)

--- __main__.py
+++ __main__.py
@@ -52,33 +52,40 @@ class TerminalBar:
         h = r.get_height()+self.spacing*2
         return pygame.Rect(0, self.win.get_height()-h, self.win.get_width(), h).collidepoint(x, y)
 
-clock = pygame.time.Clock()
-welcome = title.render('Welcome to AIHub! :)', 2, BLACK)
-btngen = lambda txt, col, txtcol=BLACK: Button(WIN, txt, col, txtcol, font=font)
-btns = [btngen('Start', GREEN), btngen('Tutorial', RED)]
-TB = TerminalBar(WIN, codefont)
-run = True
-while run:
-    WIN.fill(WHITE)
-    updates = [btns[i].update(0, 0 + sum([btns[j].nsurface.get_height() + 20 for j in range(i)])) for i in range(len(btns))]
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            break
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                run = False
-                break
-            elif TB.active != -1:
-                TB.pressed(event)
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == pygame.BUTTON_LEFT:
-                for i in [btns[j] for j in range(len(btns)) if updates[j]]:
-                    print(i)
-                TB.toggleactive(not TB.collides(*event.pos))
-    WIN.blit(welcome, (WIN.get_width()/2-welcome.get_width()/2, WIN.get_height()/2-welcome.get_height()/2))
-    TB.update()
-    pygame.display.flip()
-    clock.tick(60)
+class Game:
+    def __init__(self):
+        self.clock = pygame.time.Clock()
+        self.TB = TerminalBar(WIN, codefont)
+    def welcome(self):
+        welcome = title.render('Welcome to AIHub! :)', 2, BLACK)
+        btngen = lambda txt, col, txtcol=BLACK: Button(WIN, txt, col, txtcol, font=font, max_width=300)
+        btns = [btngen('Start', GREEN), btngen('Tutorial', RED)]
+        run = True
+        while run:
+            WIN.fill(WHITE)
+            updates = [btns[i].update(0, 0 + sum([btns[j].nsurface.get_height() + 20 for j in range(i)])) for i in range(len(btns))]
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    break
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        run = False
+                        break
+                    elif self.TB.active != -1:
+                        self.TB.pressed(event)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == pygame.BUTTON_LEFT:
+                        for i in [btns[j] for j in range(len(btns)) if updates[j]]:
+                            print(i)
+                        self.TB.toggleactive(not self.TB.collides(*event.pos))
+            WIN.blit(welcome, (WIN.get_width()/2-welcome.get_width()/2, WIN.get_height()/2-welcome.get_height()/2))
+            self.TB.update()
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    g = Game()
+    g.welcome()
 
 pygame.quit()
