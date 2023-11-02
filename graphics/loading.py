0e0a0ba62a5b063a84f3f5bb8fc509ac33ff839b
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-11-02 18:24:40 +1100

    style(GUI): 🚸 Made the loading much more beginner-friendly :)
    Moved the class to be automatically generated by the loading function and :)

--- graphics/loading.py
+++ graphics/loading.py
@@ -31,12 +31,17 @@ class thread_with_exception(Thread):
             print('Exception raise failure')
 
 def Loading(func):
-    def func2(self, WIN, font):
+    def func2(WIN, font):
+        class main:
+            def __call__(self, *args):
+                func(self, *args)
+        
+        m = main()
         t = font.render('Loading...', 2, (0, 0, 0))
         WIN.fill((255, 255, 255))
         WIN.blit(t, (WIN.get_width()//2-t.get_width()//2, 0))
         pygame.display.flip()
-        t = thread_with_exception(func, self)
+        t = thread_with_exception(m)
         t.start()
         run = True
         while t.is_alive() and run:
@@ -50,20 +55,17 @@ def Loading(func):
                         pygame.quit()
         end = t.is_alive()
         t.raise_exception()
-        return (not end), t.retargs
+        return (not end), {i:getattr(m,i) for i in dir(m) if i not in dir(main)}
     return func2
 
 if __name__ == '__main__':
-    class Main:
-        def __init__(self): self.i = -1
-        @Loading
-        def f(self):
-            for self.i in range(10):
-                sleep(1)
+    @Loading
+    def f(self):
+        for self.i in range(10):
+            sleep(1)
     pygame.init()
     WIN = pygame.display.set_mode()
     font = pygame.font.Font(None, 64)
-    m = Main()
-    ret = m.f(WIN, font)
-    print('Ran for %i seconds%s' % (m.i, (' Successfully! :)' if ret[0] else ' And failed :(')))
+    succeeded, ret = f(WIN, font)
+    print('Ran for %i seconds%s' % (ret['i'], (' Successfully! :)' if succeeded else ' And failed :(')))
     print('end')
