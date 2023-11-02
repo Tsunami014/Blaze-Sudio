3da5c680be10e647d2f6d897b4357ba78642a7f4
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-11-03 08:22:57 +1100

    feat(GUI): ✨ Made a new class for the loading screen

--- graphics/loading.py
+++ graphics/loading.py
@@ -1,6 +1,7 @@
 from threading import Thread, _active
 from time import sleep
 import ctypes, pygame
+from random import random
 
 class thread_with_exception(Thread):
     def __init__(self, target, *args):
@@ -30,17 +31,27 @@ class thread_with_exception(Thread):
             ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
             print('Exception raise failure')
 
+class LoadingScreen:
+    def __init__(self, WIN, font):
+        self.WIN = WIN
+        self.font = font
+    def update(self):
+        try:
+            self.WIN.fill((255, 255, 255))
+            t = self.font.render('Loading...', 2, (0, 0, 0))
+            self.WIN.blit(t, (self.WIN.get_width()//2-t.get_width()//2, 0))
+            pygame.display.flip()
+        except: pass
+
 def Loading(func):
     def func2(WIN, font):
         class main:
             def __call__(self, *args):
                 func(self, *args)
         
+        ls = LoadingScreen(WIN, font)
+        ls.update()
         m = main()
-        t = font.render('Loading...', 2, (0, 0, 0))
-        WIN.fill((255, 255, 255))
-        WIN.blit(t, (WIN.get_width()//2-t.get_width()//2, 0))
-        pygame.display.flip()
         t = thread_with_exception(m)
         t.start()
         run = True
@@ -53,6 +64,7 @@ def Loading(func):
                     if event.key == pygame.K_ESCAPE:
                         run = False
                         pygame.quit()
+            ls.update()
         end = t.is_alive()
         t.raise_exception()
         return (not end), {i:getattr(m,i) for i in dir(m) if i not in dir(main)}
