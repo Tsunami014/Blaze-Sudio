1a01da510661d1674b70aded55022f7c6b9b95ef
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-11-08 08:22:16 +1100

    feat(graphics): ✨ Added Graphic class
    Easier way for coders to use graphics

---
+++ graphics/graphics_options.py
@@ -0,0 +1,17 @@
+import pygame
+
+# Colours
+CWHITE = (255, 255, 255)
+CGREEN = (10, 255, 50)
+CRED = (255, 10, 50)
+CBLUE = (10, 50, 255)
+CBLACK = (0, 0, 0)
+
+# Fonts
+FTITLE = pygame.font.SysFont('Comic Sans MS', 64, True)
+FCODEFONT = pygame.font.SysFont('Lucida Sans Typewriter', 16)
+FFONT = pygame.font.SysFont(None, 52)
+
+# Positions
+PCENTER = lambda size, sizeofobj: (round(size[0]/2-sizeofobj[0]/2), round(size[1]/2-sizeofobj[1]/2))
+PTOPCENTER = lambda size, sizeofobj: (round(size[0]/2-sizeofobj[0]/2), 0)
