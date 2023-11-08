1fd20cd7ece21b4d608695e2022be4dced59b0c7
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-11-09 08:04:40 +1100

    fix(GUI): 🐛 Fixed import bugs with GUI files

--- graphics/GUI/__init__.py
+++ graphics/GUI/__init__.py
@@ -4,7 +4,13 @@ try:
     from graphics.GUI.textboxify import *
     from graphics.GUI.randomGUIelements import *
 except ImportError:
-    from inputbox import RESIZE_W, RESIZE_H, RESIZE_NONE, InputBox, renderTextCenteredAt
-    from pyguix import *
-    from textboxify import *
-    from randomGUIelements import *
+    try:
+        from GUI.inputbox import RESIZE_W, RESIZE_H, RESIZE_NONE, InputBox, renderTextCenteredAt
+        from GUI.pyguix import *
+        from GUI.textboxify import *
+        from GUI.randomGUIelements import *
+    except ImportError:
+        from inputbox import RESIZE_W, RESIZE_H, RESIZE_NONE, InputBox, renderTextCenteredAt
+        from pyguix import *
+        from textboxify import *
+        from randomGUIelements import *
