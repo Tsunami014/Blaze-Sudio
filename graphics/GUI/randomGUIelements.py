d39350ed8961d5a77fc14319f33394c74083c469
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-11-09 08:06:06 +1100

    feat(graphics): ✨ Turned button into a function and implemented it in graphics.py :)

--- graphics/GUI/randomGUIelements.py
+++ graphics/GUI/randomGUIelements.py
@@ -32,46 +32,15 @@ def renderTextCenteredAt(text, font, allowed_width): # modified from https://sta
         lines.append(line)
     return lines
 
-class Button:
-    def __init__(self, screen, txt, colour, txtcolour=(255, 255, 255), max_width=100, font=pygame.font.Font(None, 24), roundness=8, on_hover_enlarge=-1):
-        self.roundness = roundness
-        self.onHoverEnlarge = on_hover_enlarge
-        self.colour = colour
-        self.max_width = max_width
-        self.txt = txt
-        self.font = font
-        self.txtcolour = txtcolour
-        self.screen = pygame.Surface((0, 0))
-        self.update(0, 0)
-        self.screen = screen
-    
-    def __str__(self):
-        return 'Button saying "%s"' % self.txt
-    
-    def __repr__(self):
-        return str(self)
-    
-    def update(self, x, y):
-        """
-        draws the button to the screen, and returns whether the user has their mouse over it. So if the mousedown is also there, then they clicked it.
+def Button(txt, colour, txtcolour=(255, 255, 255), max_width=100, font=pygame.font.Font(None, 24), onHoverEnlarge=-1):
+    lines = [font.render(line, True, txtcolour) for line in renderTextCenteredAt(txt, font, max_width)]
 
-        Returns
-        -------
-        bool
-            whether or not the user has their mouse ___***OVER***___ the button, NOT CLICKED.
-        """
-        lines = [self.font.render(line, True, self.txtcolour) for line in renderTextCenteredAt(self.txt, self.font, self.max_width)]
-
-        self.nsurface = pygame.Surface((max([i.get_width() for i in lines]), sum([i.get_height() for i in lines])+(len(lines)-1)*10))
-        self.nsurface.fill(self.colour)
-        top = 0
-        for i in lines:
-            self.nsurface.blit(i, (0, top))
-            top += i.get_height()+10
-
-        btn = pygame.Rect(x, y, self.nsurface.get_width() + 20, self.nsurface.get_height() + 20)
-        if self.onHoverEnlarge != -1 and btn.collidepoint(pygame.mouse.get_pos()):
-            btn = pygame.Rect(x-self.onHoverEnlarge, y-self.onHoverEnlarge, self.nsurface.get_width() + 20 + self.onHoverEnlarge*2, self.nsurface.get_height() + 20 + self.onHoverEnlarge*2)
-        pygame.draw.rect(self.screen, self.colour, btn, border_radius=self.roundness)
-        self.screen.blit(self.nsurface, (x+10, y+10))
-        return btn.collidepoint(pygame.mouse.get_pos())
+    nsurface = pygame.Surface((max([i.get_width() for i in lines]), sum([i.get_height() for i in lines])+(len(lines)-1)*10))
+    nsurface.fill(colour)
+    top = 0
+    for i in lines:
+        nsurface.blit(i, (0, top))
+        top += i.get_height()+10
+    
+    btnsur = pygame.Rect(0, 0, nsurface.get_width() + 20, nsurface.get_height() + 20)
+    return btnsur, nsurface
--- graphics/graphics.py
+++ graphics/graphics.py
@@ -2,33 +2,60 @@ import pygame
 pygame.init()
 try:
     import graphics.graphics_options as GO
+    from graphics.GUI.randomGUIelements import Button
 except:
     import graphics_options as GO
+    from GUI.randomGUIelements import Button
 
 class Graphic:
     def __init__(self):
         self.WIN = pygame.display.set_mode()
         self.clock = pygame.time.Clock()
         self.statics = []
+        self.buttons = []
     def set_caption(caption):
         pygame.display.set_caption(caption)
     
     def render(self, func=None):
-        self.WIN.fill((255, 255, 255))
-        for i in self.statics:
-            self.WIN.blit(i[0], i[1])
+        s = pygame.Surface(self.WIN.get_size())
+        s.fill((255, 255, 255))
         if func != None: func(True)
-        pygame.display.flip()
+        for i in self.statics:
+            s.blit(i[0], i[1])
+        return s
     
     def graphic(self, func):
         def func2():
-            prevs = [self.statics.copy()]
+            prevs = [self.statics.copy(), self.buttons.copy()]
             run = True
-            self.render(func)
+            s = self.render(func)
             while run:
-                if prevs != [self.statics]:
-                    self.render(func)
-                    prevs = [self.statics.copy()]
+                if prevs != [self.statics, self.buttons]:
+                    s = self.render(func)
+                    prevs = [self.statics.copy(), self.buttons.copy()]
+                self.WIN.fill((255, 255, 255))
+                self.WIN.blit(s, (0, 0))
+                touchingbtns = []
+                for btn in self.buttons:
+                    r, sur = Button(*btn[0])
+                    sze = btn[1](self.WIN.get_size(), r.size)
+                    r.move_ip(*sze)
+                    col = r.collidepoint(pygame.mouse.get_pos())
+                    if btn[0][-1] != -1 and col:
+                        r = pygame.Rect(-btn[0][-1], -btn[0][-1], sur.get_width() + 20 + btn[0][-1], sur.get_height() + 20 + btn[0][-1])
+                        r.move_ip(*sze)
+                    pygame.draw.rect(self.WIN, btn[0][1], r, border_radius=8)
+                    self.WIN.blit(sur, (sze[0]+10, sze[1]+10))
+                    if col: touchingbtns.append(btn)
+                for btn in touchingbtns: # repeat so the buttons you are touching appear on top
+                    r, sur = Button(*btn[0])
+                    sze = btn[1](self.WIN.get_size(), r.size)
+                    r.move_ip(*sze)
+                    if btn[0][-1] != -1:
+                        r = pygame.Rect(-btn[0][-1], -btn[0][-1], sur.get_width() + 20 + btn[0][-1]*2, sur.get_height() + 20 + btn[0][-1]*2)
+                        r.move_ip(*sze)
+                    pygame.draw.rect(self.WIN, btn[0][1], r, border_radius=8)
+                    self.WIN.blit(sur, (sze[0]+10, sze[1]+10))
                 run = func(False)
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
@@ -38,12 +65,20 @@ class Graphic:
                         if event.key == pygame.K_ESCAPE:
                             run = False
                             return
+                    elif event.type == pygame.MOUSEBUTTONDOWN:
+                        if event.button == pygame.BUTTON_LEFT:
+                            for i in touchingbtns:
+                                func(i)
+                pygame.display.flip()
                 self.clock.tick(60)
         return func2
     
-    def add_text(self, txt, colour, font, position):
+    def add_text(self, txt, colour, position, font=GO.FFONT):
         obj = font.render(txt, 2, colour)
         self.statics.append((obj, position(self.WIN.get_size(), obj.get_size())))
+    
+    def add_button(self, txt, col, position, txtcol=GO.CBLACK, font=GO.FFONT, on_hover_enlarge=True):
+        self.buttons.append(((txt, col, txtcol, 900, font, (-1 if on_hover_enlarge==False else (10 if on_hover_enlarge==True else on_hover_enlarge))), position))
     
     def clear(self):
         self.statics = []
@@ -52,11 +87,14 @@ if __name__ == '__main__':
     G = Graphic()
     @G.graphic
     def test(ui):
-        if ui:
-            G.add_text('HI :)', GO.CGREEN, GO.FTITLE, GO.PTOPCENTER)
-            G.add_text('This is a cool thing', GO.CBLUE, GO.FFONT, GO.PCENTER)
-        else:
+        if ui == True: # Load the graphics
+            G.add_text('HI :)', GO.CGREEN, GO.PTOPCENTER, GO.FTITLE)
+            G.add_text('This is a cool thing', GO.CBLUE, GO.PCENTER)
+            G.add_button('Button 1 :D', GO.CBLUE, GO.PBOTTOMCENTER)
+        elif ui == False: # This runs every 1/60 secs
             pass
+        else: # Some UI element got clicked!
+            print(ui)
         return True
     test()
     pygame.quit() # this here for very fast quitting
