89c4e000b35fad634b4e97af914134f3f57425de
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-11-09 16:13:09 +1100

    fix(graphics): 🐛 MADE EVERYTHING WORK!!!
    Added an 'empty space' class and fixed the pos_store func SO NOW IT WORKS :D :D :D

--- graphics/graphics.py
+++ graphics/graphics.py
@@ -76,6 +76,13 @@ class Graphic:
         pos = self.pos_store(GO.PSTACKS[position][1](self.WIN.get_size(), obj.get_size()), obj.get_size(), position)
         self.statics.append((obj, pos))
     
+    def add_empty_space(self, position, wid, hei):
+        empty = (255,255,255,0) 
+        obj = pygame.Surface((wid, hei))
+        obj.fill(empty)
+        pos = self.pos_store(GO.PSTACKS[position][1](self.WIN.get_size(), obj.get_size()), obj.get_size(), position)
+        self.statics.append((obj, pos))
+    
     def add_button(self, txt, col, position, txtcol=GO.CBLACK, font=GO.FFONT, on_hover_enlarge=True):
         self.buttons.append(((txt, col, txtcol, 900, font, (-1 if on_hover_enlarge==False else (10 if on_hover_enlarge==True else on_hover_enlarge))), position))
     
@@ -84,11 +91,13 @@ class Graphic:
         if func not in self.store:
             self.store[func] = [sze[0]*sizeing[0], sze[1]*sizeing[1]]
             return pos
-        pos = [self.store[func][0]+pos[0]+10*sizeing[0], self.store[func][1]+pos[1]+10*sizeing[1]]
-        self.store[func] = [pos[0]+sze[0]*sizeing[0], pos[1]+sze[1]*sizeing[1]]
-        if sizeing[0] < 0: pos[0] += sze[0]*sizeing[0]
-        if sizeing[1] < 0: pos[1] += sze[1]*sizeing[0]
-        return pos
+        pos2 = [None, None]
+        if pos[0] < 0: pos2[0] = pos[0]+(0-self.store[func][0])*sizeing[0]
+        else: pos2[0] = pos[0]+self.store[func][0]
+        if pos[1] < 0: pos2[1] = pos[1]+(0-self.store[func][1])*sizeing[1]
+        else: pos2[1] = pos[1]+self.store[func][1]
+        self.store[func] = [self.store[func][0] + sze[0]*sizeing[0], self.store[func][1] + sze[1]*sizeing[1]]
+        return pos2
     
     def clear(self):
         self.statics = []
@@ -102,13 +111,14 @@ if __name__ == '__main__':
         if ui == True: # Load the graphics
             G.clear()
             G.add_text('HI', GO.CGREEN, GO.PRBOTTOM, GO.FTITLE)
-            G.add_text(':)', GO.CBLACK, GO.PRBOTTOM, GO.FTITLE)
+            G.add_text(':) ', GO.CBLACK, GO.PRBOTTOM, GO.FTITLE)
             G.add_text('This is a cool thing', GO.CBLUE, GO.PCCENTER)
             G.add_text('Sorry, I meant a cool TEST', GO.CRED, GO.PCCENTER)
+            G.add_empty_space(GO.PCBOTTOM, 10, 20)
             G.add_button('Button 1 :D', GO.CYELLOW, GO.PCBOTTOM)
-            G.add_button('Button 2 :(', GO.CBLUE, GO.PCBOTTOM)
-            G.add_text('Are you', GO.CBLACK, GO.PLTOP)
-            G.add_text('happy?', GO.CGREEN, GO.PLTOP)
+            G.add_button('Button 2 :(  hi', GO.CBLUE, GO.PCBOTTOM)
+            G.add_text('Are you ', GO.CBLACK, GO.PLTOP)
+            G.add_text('happy? ', GO.CGREEN, GO.PLTOP)
             G.add_text('Or sad?', GO.CRED, GO.PLTOP)
         elif ui == False: # This runs every 1/60 secs
             pass
