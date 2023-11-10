f1f9c205a65cf256d0c1fe59d6a6d853cf601f70
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-11-10 15:58:25 +1100

    feat(GUI): 💄 Added more things!
    Added 1 new colour, 1 new position and the ability to generate new positions!

--- graphics/graphics_options.py
+++ graphics/graphics_options.py
@@ -7,6 +7,7 @@ CRED = (255, 10, 50)
 CBLUE = (10, 50, 255)
 CBLACK = (0, 0, 0)
 CYELLOW = (255, 200, 50)
+CGREY = (125, 125, 125)
 
 # Fonts
 FTITLE = pygame.font.SysFont('Comic Sans MS', 64, True)
@@ -23,6 +24,7 @@ PCBOTTOM = 5
 PRTOP = 6
 PRCENTER = 7
 PRBOTTOM = 8
+PFILL = 9
 
 # Stacks. Don't use unless you know what you're doing
 PSTACKS = {
@@ -34,8 +36,18 @@ PSTACKS = {
     PCBOTTOM: ([0, -1], lambda size, sizeofobj: (round(size[0]/2-sizeofobj[0]/2), size[1]-sizeofobj[1])),
     PRTOP:    ([-1, 0], lambda size, sizeofobj: (size[0]-sizeofobj[0], 0)),
     PRCENTER: ([-1, 0], lambda size, sizeofobj: (size[0]-sizeofobj[0], round(size[1]/2-sizeofobj[1]/2))),
-    PRBOTTOM: ([-1, 0], lambda size, sizeofobj: (size[0]-sizeofobj[0], size[1]-sizeofobj[1]))
+    PRBOTTOM: ([-1, 0], lambda size, sizeofobj: (size[0]-sizeofobj[0], size[1]-sizeofobj[1])),
+    PFILL:    ([0, 0],  lambda size, sizeofobj: (0, 0))
 }
+
+PIDX = 0 # DO NOT USE UNLESS YOU REALLY KNOW WHAT YOU'RE DOING
+
+def PNEW(stack, func, idx=None): # To create new layouts
+    if idx == None:
+        idx = PIDX
+        PIDX += 1
+    PSTACKS[idx+10] = (stack, func)
+    return idx+10
 
 # Events
 TFIRST = 0
