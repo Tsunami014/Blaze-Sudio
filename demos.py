66b7f7dbb11ec236d9aeaa4ba5fbf8e5e57dd26f
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-11-16 15:47:04 +1100

    feat(other): 🚚 Added async_handling demo to demos.py

--- demos.py
+++ demos.py
@@ -111,6 +111,27 @@ def terrainGenDemo():
     print(outs[0])
     pass
 
+def async_handlingDemo(): # TODO: Improve this demo
+    import random, asyncio, pygame
+    from graphics import Progressbar
+    async def wait_random():
+        seconds = random.randint(200, 2000) / 100
+        #print(f"Waiting for {seconds} seconds...")
+        await asyncio.sleep(seconds)
+        #print(f"Done waiting for {seconds} seconds!")
+        return seconds
+    
+    pygame.init()
+    WIN = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Loading Bar Example")
+    WIN.fill((255, 255, 255))
+    pygame.display.update()
+    bar = Progressbar(600, 50)
+    tasks = [wait_random() for _ in range(500)]
+    print(bar(WIN, (WIN.get_width() - 600) // 2, (WIN.get_height() - 50) // 2, 5, tasks))
+    asyncio.get_event_loop().stop()
+    pygame.quit()
+
 if __name__ == '__main__':
     root = Tk.Tk()
     def cmd(cmdd):
@@ -120,4 +141,5 @@ if __name__ == '__main__':
     Tk.Button(root, text='Graphics Demo', command=lambda: cmd(GraphicsDemo)).pack()
     Tk.Button(root, text='Generate World Demo', command=lambda: cmd(worldsDemo)).pack()
     Tk.Button(root, text='Generate Terrain Demo', command=lambda: cmd(terrainGenDemo)).pack()
+    Tk.Button(root, text='Async Progressbar Demo', command=lambda: cmd(async_handlingDemo)).pack()
     root.mainloop()
