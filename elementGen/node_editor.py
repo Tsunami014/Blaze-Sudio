38cafcc152c1a3f1916fdc8051ff77e71e10b3b6
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-11-17 08:55:50 +1100

    feat(GUI): ✨ Put the Graphic Demo into node_editor

--- elementGen/node_editor.py
+++ elementGen/node_editor.py
@@ -1,3 +1,7 @@
+import graphics.graphics_options as GO
+import pygame
+from time import sleep
+
 def NodeEditor(G):
     """Makes a Node Editor screen! Still in progress. Come back later!
 
@@ -13,4 +17,68 @@ G = Graphic()
 NodeEditor(G)
 ```
     """
-    pass
+    @G.Loading
+    def test_loading(self):
+        for self.i in range(10):
+            sleep(1)
+    
+    @G.Graphic
+    def test(event, txt, element=None, aborted=False): # You do not need args and kwargs if you KNOW that your function will not take them in. Include what you need.
+        if event == GO.EFIRST: # First, before anything else happens in the function
+            G.Container.txt = txt
+        if event == GO.ELOADUI: # Load the graphics
+            CTOP = GO.PNEW([1, 0], GO.PSTACKS[GO.PCTOP][1], 0) # Bcos usually the Center Top makes the elements stack down, so I make a new thing that stacks sideways
+            G.Clear()
+            G.add_text('HI', GO.CGREEN, GO.PRBOTTOM, GO.FTITLE)
+            G.add_text(':) ', GO.CBLACK, GO.PRBOTTOM, GO.FTITLE)
+            G.add_empty_space(GO.PCCENTER, 0, -150) # Yes, you can have negative space. This makes the next things shifted the other direction.
+            G.add_text('This is a cool thing', GO.CBLUE, GO.PCCENTER)
+            G.add_text('Sorry, I meant a cool TEST', GO.CRED, GO.PCCENTER)
+            G.add_text(G.Container.txt, GO.CGREEN, GO.PCCENTER)
+            G.add_empty_space(GO.PCBOTTOM, 0, 20)
+            G.add_button('Button 1 :D', GO.CYELLOW, GO.PCBOTTOM)
+            G.add_text('Buttons above [^] and below [v]', GO.CBLUE, GO.PCBOTTOM)
+            G.add_button('Textbox test', GO.CBLUE, GO.PCBOTTOM)
+            G.add_button('Loading test', GO.CGREEN, GO.PCBOTTOM)
+            G.Container.exitbtn = G.add_button('EXIT', GO.CRED, GO.PCBOTTOM)
+            G.add_empty_space(CTOP, -150, 0) # Center it a little more
+            G.add_text('Are you ', GO.CBLACK, CTOP)
+            G.add_text('happy? ', GO.CGREEN, CTOP)
+            G.add_text('Or sad?', GO.CRED, CTOP)
+        elif event == GO.ETICK: # This runs every 1/60 secs (each tick)
+            return True # Return whether or not the loop should continue.
+        elif event == GO.EELEMENTCLICK: # Some UI element got clicked!
+            if element.type == GO.TBUTTON:
+                # This gets passed 'element': the element that got clicked. TODO: make an Element class
+                # The == means element's uid == __
+                # UID gets generated based off order: so UID of 2 means second thing created that makes a UID.
+                # When you create a thing that makes a UID it returns it. e.g. button1 = G.add_button(etc.)
+                # So in that example button1 is the UID. Maybe try saving it to the container tho! Example shown by the exit button.
+                if element == 2:
+                    succeeded, ret = test_loading()
+                    G.Container.txt = ('Ran for %i seconds%s' % (ret['i']+1, (' Successfully! :)' if succeeded else ' And failed :(')))
+                    G.Reload()
+                elif element == G.Container.exitbtn:
+                    G.Abort()
+                elif element == 1:
+                    bot = GO.PNEW([0, 0], GO.PSTACKS[GO.PCBOTTOM][1], 1)
+                    G.add_TextBox('HALLOOOO! :)', bot)
+                    G.Container.idx = 0
+                else:
+                    G.Container.txt = element.txt # put name of button in middle
+                    G.Reload()
+            elif element.type == GO.TTEXTBOX:
+                if G.Container.idx == 0:
+                    element.set_text("Happy coding!")
+                    G.Container.idx = 1
+                else:
+                    element.remove()
+        elif event == GO.EEVENT: # When something like a button is pressed. Is passed 'element' too, but this time it is an event
+            if element.type == pygame.KEYDOWN:
+                if element.key == pygame.K_SPACE:
+                    G.Container.txt = 'You pressed space!'
+                    G.Reload()
+        elif event == GO.ELAST:
+            # This also gets passed 'aborted': Whether you aborted or exited the screen
+            return aborted # Whatever you return here will be returned by the function
+    return test()
