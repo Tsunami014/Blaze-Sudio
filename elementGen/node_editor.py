5ebdc5770bdfd4ab88c92c772d6a2d91b0a3a4ee
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-11-17 08:28:31 +1100

    feat(GUI): ✨ Made the basic NodeEditor function! Still does nothing yet.
    But also added docstring!

---
+++ elementGen/node_editor.py
@@ -0,0 +1,16 @@
+def NodeEditor(G):
+    """Makes a Node Editor screen! Still in progress. Come back later!
+
+    Parameters
+    ----------
+    G : graphics.Graphic
+        The Graphic screen
+    
+    USE:
+```py
+from graphics import Graphic
+G = Graphic()
+NodeEditor(G)
+```
+    """
+    pass
