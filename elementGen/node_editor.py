de61c382938aab0ecc4559f4901bc5f43f07cf80
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-11-22 08:07:53 +1100

    feat(main): ✨ Added first mouse press to node editor

--- elementGen/node_editor.py
+++ elementGen/node_editor.py
@@ -1,4 +1,105 @@
-def NodeEditor(G):
+import graphics.graphics_options as GO
+import pygame, os, json
+from time import sleep
+
+categories = [i.name for i in os.scandir('data/elements') if i.is_dir()]
+
+def mouseDown(button=1):
+    i = False
+    while True:
+        r = pygame.mouse.get_pressed(3)[button-1]
+        yield ((i != r and r), r)
+        i = r
+
+def NodeSelector(G, continue_to_edit=0):
+    """Makes a Node Selector screen! Still in progress. Come back later!
+
+    Parameters
+    ----------
+    G : graphics.Graphic
+        The Graphic screen
+    continue_to_edit : int, optional
+        Whether to, once selected, return (0 & default), run the editor and exit once the editor gets exited (1) OR go back into select dialog when exit (2)
+    
+    USE:
+```py
+from graphics import Graphic
+G = Graphic()
+NodeSelector(G)
+```
+    """
+    @G.Graphic
+    def item_select(event, category, element=None, aborted=False):
+        if event == GO.EFIRST:
+            @G.Loading
+            def load(self):
+                self.items = [i for i in os.scandir('data/elements/'+category) if i.is_file()]
+                self.iteminfo = [json.load(open('data/elements/%s/%s'%(category, i.name))) for i in self.items]
+                self.subs = ['Go back to the previous page', 'Make a new item from scratch'] + [i['idea'] for i in self.iteminfo]
+            cont, res = load()
+            G.Container.res = res
+            G.Container.txt = ''
+            G.Container.prevpresses = []
+            if not cont: G.Abort()
+        elif event == GO.ELOADUI:
+            G.Clear()
+            G.add_text(category + ' item selection', GO.CBLACK, GO.PCTOP)
+            G.add_text(G.Container.txt, GO.CBLUE, GO.PCTOP)
+            G.add_button('Back', GO.CGREY, GO.PLTOP)
+            G.add_button('New Item', GO.CGREEN, GO.PLTOP)
+            cols = GO.CRAINBOW()
+            for i in G.Container.res['iteminfo']:
+                G.add_button(i['name'], next(cols), GO.PLCENTER)
+        elif event == GO.ETICK:
+            if G.touchingbtns != G.Container.prevpresses:
+                G.Container.prevpresses = G.touchingbtns.copy()
+                try: G.Container.txt = G.Container.res['subs'][G.get_idx(G.touchingbtns[0])]
+                except: G.Container.txt = ''
+                G.Reload()
+            return True
+        elif event == GO.EELEMENTCLICK: # Passed 'element'
+            if element == 0: # back
+                return False
+            elif element == 1: # make new world
+                return 'NEW'
+            else:
+                return G.Container.res['items'][element.uid-2].name
+    @G.Graphic
+    def category_select(event, element=None, aborted=False):
+        if event == GO.ELOADUI:
+            G.Clear()
+            G.add_text('Category selection', GO.CBLACK, GO.PCTOP)
+            G.add_button('Back', GO.CGREY, GO.PLTOP)
+            G.add_button('New Category', GO.CGREEN, GO.PLTOP)
+            cols = GO.CRAINBOW()
+            for i in categories:
+                G.add_button(i, next(cols), GO.PLCENTER)
+        elif event == GO.ETICK:
+            return True
+        elif event == GO.EELEMENTCLICK: # Passed 'element'
+            if element == 0: # back
+                return None
+            elif element == 1: # make new world
+                if continue_to_edit == 1 or continue_to_edit == 2:
+                    NodeEditor(G, 'NEW/NEW')
+                if continue_to_edit != 2:
+                    return 'NEW/NEW'
+            else:
+                name = element.txt
+                selection = item_select(name)
+                if selection != False and selection != None:
+                    pth = name + '/' + selection
+                    if continue_to_edit == 1 or continue_to_edit == 2:
+                        NodeEditor(G, pth)
+                    if continue_to_edit != 2:
+                        return pth
+    return category_select()
+
+# Select element to edit screen (copy world select)
+# Each category of elements is a sub-folder under data/elements/
+# As well as a NodeEditor screen have a NodeRenderer screen, which is also used in NodeEditor
+
+def NodeEditor(G, path):
     """Makes a Node Editor screen! Still in progress. Come back later!
 
     Parameters
@@ -17,6 +118,10 @@ NodeEditor(G)
     def editor(event, path, element=None, aborted=False):
         if event == GO.EFIRST:
             G.Container.saved = False
+            G.Container.md = [
+                mouseDown(), # Left mouse button
+                mouseDown(3) # Right mouse button
+            ]
             if path.endswith('.elm'):
                 path = path[:-4]
             if not os.path.exists('data/elements/'+path+'.elm'):
@@ -44,19 +149,28 @@ NodeEditor(G)
                 elif event == GO.ETICK: return True
                 elif event == GO.EELEMENTCLICK:
                     if element == G.Container.go:
-                        print('GO!')
+                        G.Container.went = True
                         G.Abort()
                     elif element == G.Container.exit:
-                        print('Cancel. :(')
+                        G.Container.went = False
                         G.Abort()
                 elif event == GO.ELAST:
-                    res = G.uids[G.Container.inpname].text
-                    if res != '':
-                        G.Container.name = res
-                        G.Container.contents['name'] = res
-                    pass # Whatever you return here will be returned by the function
+                    if G.Container.went: # Not cancelled
+                        res = G.uids[G.Container.inpname].text
+                        if res != '':
+                            G.Container.name = res
+                            G.Container.contents['name'] = res
+                        pass # Whatever you return here will be returned by the function
             settings()
         elif event == GO.ETICK:
+            lf, l = next(G.Container.md[0])
+            # lf = left mouse button first press, l = left mouse button is being pressed
+            rf, r = next(G.Container.md[1])
+            # Same with r
+            if lf:
+                pygame.draw.circle(G.WIN, GO.CGREEN, pygame.mouse.get_pos(), 20)
+            elif l:
+                pygame.draw.circle(G.WIN, GO.CNEW('orange'), pygame.mouse.get_pos(), 10)
             return True
         elif event == GO.EEVENT: # When something like a button is pressed. Is passed 'element' too, but this time it is an event
             if element.type == pygame.KEYDOWN:
