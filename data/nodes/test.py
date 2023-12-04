931dceee58bd2181411097868ed84bc3eac969b4
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-12-05 07:57:06 +1100

    todo: rebase

--- data/nodes/test.py
+++ data/nodes/test.py
@@ -1,7 +1,8 @@
 # Add int@A int@B | int@Out #
 
 def node(A, B):
-    return {'Out': A + B}
+    return {'Out': A + B} # How this works is the dict is the outputs' names to replace by a new value
+    # So in this case the output with the name 'Out' gets shown as A + B instead of it's original 'Out' value if this doesn't fail
 
 # One | int@1 #
 def node():
