4907c9e3daf1de448fe19b6861b93ebf2af38708
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-12-06 20:21:36 +1100

    Made it check many previous messages in discord for the one with an @ in it

--- .github/scripts/notifyDiscord.js
+++ .github/scripts/notifyDiscord.js
@@ -16,52 +16,46 @@ function processMessage(message, user) {
 
 let previousMessage = null;
 
-client.once(Events.ClientReady, (readyClient) => {
+client.once(Events.ClientReady, async (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 
   const channel = readyClient.channels.cache.get('1181875066280091688'); // Replace with your channel ID
 
   if (channel) {
-    console.log(`We gots a channel :)`);
-    channel.messages.fetch({ limit: 1 })
-      .then(messages => {
-        previousMessage = messages.last(); // Get the second-to-last message
-        console.log(`Previous message: ${previousMessage.content}`);
-        // Send a new message in the same channel
-        const oldMsg = previousMessage.content;
-        const githubActor = process.env.GITHUB_ACTOR;
-        console.log(`GitHub actor: ${githubActor}`);
-        const newMsg = processMessage(oldMsg, githubActor)
-        channel.send(newMsg)
-          .then(() => {
-            console.log(`Sent message: ${newMsg}`)
-            process.exit(); // Exit the process after sending the message
-          })
-          .catch(error => {
-            console.error(`Error sending message: ${error}`);
-            process.exit(); // Exit the process after sending the message
-          });
-      })
-      .catch(error => {
-        console.error(`Error fetching messages: ${error}`)
-        process.exit(); // Exit the process after sending the message
-      });
-  }
-});
+    console.log(`We got a channel :)`);
 
-/*client.on('messageCreate', (message) => {
-  const commitAuthor = process.env.GITHUB_ACTOR;
+    try {
+      const messages = await channel.messages.fetch({ limit: 20 }); // Fetch up to 10 previous messages
 
-  if (message.content.includes(`${commitAuthor} is working`)) {
-    if (message.author.username === commitAuthor) {
-      // If the message contains the commit author and it's the same person, reply with 'really'
-      message.reply('really');
-    } else {
-      // If the message contains the commit author but it's a different person, reply with 'hard.\n{0} is working'
-      const newPerson = message.author.username;
-      message.reply(`hard.\n${newPerson} is working`);
+      // Iterate through the fetched messages
+      for (const [, message] of messages) {
+        if (message.content.includes('@')) {
+          // Check if the message contains an '@' mention
+          const githubActor = process.env.GITHUB_ACTOR;
+          console.log(`GitHub actor: ${githubActor}`);
+          const newMsg = processMessage(message.content, githubActor);
+          channel.send(newMsg)
+            .then(() => {
+              console.log(`Sent message: ${newMsg}`);
+              process.exit(); // Exit the process after sending the message
+            })
+            .catch(error => {
+              console.error(`Error sending message: ${error}`);
+              process.exit(); // Exit the process after sending the message
+            });
+          break; // Exit the loop once a message with an '@' mention is found
+        }
+      }
+
+      // If no message with an '@' mention is found
+      console.log('No message with an "@" mention found.');
+      process.exit(); // Exit the process
+
+    } catch (error) {
+      console.error(`Error fetching messages: ${error}`);
+      process.exit(); // Exit the process after sending the message
     }
   }
-});*/
+});
 
 client.login(token);
