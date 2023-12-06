6e49fd5c5037f587ef3df30f73391489f34707f1
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-12-06 16:13:48 +1100

    feat(graphics): 💥 Added the Scrollbox to Graphics!
    This is a VERY explosion feature; many important things get replaced with variables so there may be bugs but I tested and there don't appear to be any
    In any case it's just very different than before I guess

--- graphics/GUI/scrollable.py
+++ graphics/GUI/scrollable.py
@@ -1,19 +1,20 @@
 import pygame
 
 class Scrollable:
-    def __init__(self, sur, pos, goalrect):
+    def __init__(self, sur, pos, goalrect, bounds=(0, float('inf'))):
         self.sur = sur
         self.pos = pos
         self.goalrect = goalrect
         self.scroll = 0
+        self.bounds = bounds
     def update(self, event):
-        if event.type == pygame.MOUSEWHEEL:
-            y = event.y - 1
-            if 0 <= y <= 1: y = 2
-            self.scroll += y
+        y = event.y - 1
+        if 0 <= y <= 1: y = 2
+        self.scroll += y * 2
+        self.scroll = -min(max(self.bounds[0], -self.scroll), self.bounds[1])
     
     def __call__(self, WIN):
-        s = pygame.Surface((self.goalrect))
+        s = pygame.Surface(self.goalrect)
         s.blit(self.sur, (0, self.scroll))
         WIN.blit(s, self.pos)
 
