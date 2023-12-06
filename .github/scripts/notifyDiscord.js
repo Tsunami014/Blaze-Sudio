2b55a075836ca7f6c3937d5a386deba1d5c10c2c
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-12-06 19:33:40 +1100

    back to what we had before

--- .github/scripts/notifyDiscord.js
+++ .github/scripts/notifyDiscord.js
@@ -1,35 +1,37 @@
-const { Client, GatewayIntentBits, Events } = require('discord.js');
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 
+const { token } = process.env.DISCORD_TOKEN;
+
+// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-let previousMessage = null;
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+client.once(Events.ClientReady, async readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    const channel = readyClient.channels.cache.get('1181875066280091688'); // Replace with your channel ID
+    if (channel) {
+        const messages = await channel.messages.fetch({ limit: 1 });
+        previousMessage = messages.last(); // Get the second-to-last message
+    }
+    console.log(`Previous message: ${previousMessage}`)
+});
 
-client.once(Events.ClientReady, async (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-
-  // Get the previous message in a certain channel
-  const channel = readyClient.channels.cache.get('1181875066280091688'); // Replace with your channel ID
-  if (channel) {
-    const messages = await channel.messages.fetch({ limit: 1 });
-    previousMessage = messages.last(); // Get the second-to-last message
-  }
-
+/*client.on('messageCreate', (message) => {
   const commitAuthor = process.env.GITHUB_ACTOR;
 
-  // Check if the previous message contains '{0} is working'
-  if (previousMessage && previousMessage.content.includes(`${commitAuthor} is working`)) {
-    if (previousMessage.author.username === commitAuthor) {
-      // If the previous message contains the commit author and it's the same person, reply with 'really'
+  if (message.content.includes(`${commitAuthor} is working`)) {
+    if (message.author.username === commitAuthor) {
+      // If the message contains the commit author and it's the same person, reply with 'really'
       message.reply('really');
     } else {
-      // If the previous message contains the commit author but it's a different person, reply with 'hard.\n{0} is working'
-      const newPerson = previousMessage.author.username;
+      // If the message contains the commit author but it's a different person, reply with 'hard.\n{0} is working'
+      const newPerson = message.author.username;
       message.reply(`hard.\n${newPerson} is working`);
     }
   }
+});*/
 
-  // Update the previous message for the next check
-  previousMessage = message;
-});
-
-client.login(process.env.DISCORD_TOKEN);
+client.login(token);
