9fb705e5e297831e7df3a96cfa74d80975413839
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-12-12 08:54:45 +1100

    feat(graphics): ✨ Finished making the colour picker 2D! Now everything works!
    Fixed a crap tonna stuff to get here

--- graphics/GUI/colourpick.py
+++ graphics/GUI/colourpick.py
@@ -3,34 +3,39 @@ import pygame
 # Thanks to https://stackoverflow.com/questions/73517832/how-to-make-an-color-picker-in-pygame :)
 
 class ColourPicker:
-    def __init__(self, x, y):
-        self.pwidth = 360
-        self.pheight = 100
-        self.rect = pygame.Rect(x, y, self.pwidth, self.pheight)
+    def __init__(self, x, y, border=5, w=100, h=None):
+        self.pwidth = w
+        self.pheight = h or w
+        self.rect = pygame.Rect(x-border, y-border, self.pwidth+border*2, self.pheight+border*2)
+        self.imrect = pygame.Rect(x, y, self.pwidth, self.pheight)
+        self.border = border
         self.image = pygame.Surface((self.pwidth, self.pheight))
         self.image.fill((255, 255, 255))
         for i in range(self.pwidth):
             for j in range(self.pheight):
-                colour = pygame.Color(0)
-                colour.hsla = (int(360*i/self.pwidth), 100, 100-j, 100)
-                pygame.draw.rect(self.image, colour, (i, j, 1, 1))
+                try:
+                    colour = pygame.Color(0)
+                    colour.hsla = (int((360/self.pwidth)*i/2*(360/self.pwidth)), 100, 100-int((100/self.pheight)*j*2*(100/self.pheight)), 100)
+                    pygame.draw.rect(self.image, colour, (i, j, 1, 1))
+                except: pass
         self.p = (0, 0)
 
     def get_colour(self):
         colour = pygame.Color(0)
-        colour.hsla = (int(self.p[0] * self.pwidth), 100, 100-int(self.p[1] * self.pheight), 100)
+        colour.hsla = (int((360/self.pwidth)*self.pwidth*self.p[0]), 100, 100-int(100*self.p[1]), 100)
         return colour
 
     def update(self):
         mouse_buttons = pygame.mouse.get_pressed()
         mouse_pos = pygame.mouse.get_pos()
-        if mouse_buttons[0] and self.rect.collidepoint(mouse_pos):
-            self.p = ((mouse_pos[0] - self.rect.left) / self.pwidth, (mouse_pos[1] - self.rect.top) / self.pheight)
+        if mouse_buttons[0] and self.imrect.collidepoint(mouse_pos):
+            self.p = ((mouse_pos[0] - self.imrect.left) / self.pwidth, (mouse_pos[1] - self.imrect.top) / self.pheight)
             self.p = ((max(0, min(self.p[0], 1))), (max(0, min(self.p[1], 1))))
 
     def draw(self, surf):
-        surf.blit(self.image, self.rect)
-        center = self.rect.left + self.p[0] * self.pwidth, self.rect.top + self.p[1] * self.pheight
+        surf.fill((255, 255, 255), self.rect)
+        surf.blit(self.image, self.imrect)
+        center = self.imrect.left + self.p[0] * self.pwidth, self.imrect.top + self.p[1] * self.pheight
         pygame.draw.circle(surf, self.get_colour(), center, 25)
 
 if __name__ == '__main__':
@@ -38,7 +43,7 @@ if __name__ == '__main__':
     window = pygame.display.set_mode((500, 500))
     clock = pygame.time.Clock()
 
-    cp = ColourPicker(50, 50)
+    cp = ColourPicker(50, 50, 20, 200)
 
     run = True
     while run:
