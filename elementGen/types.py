f0e85288dbd5da3131b226aee4dd3c1c780e4dbb
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-12-11 16:19:19 +1100

    feat(nodes): ✨ Made more funcs and made the add able to add nums AND text!

--- elementGen/types.py
+++ elementGen/types.py
@@ -1,19 +1,25 @@
+from typing import Any
+
 types = {
     'int': int,
-    'str': str
+    'str': str,
+    'any': Any
 }
 
 strtypes = {
     int: 'int',
-    str: 'str'
+    str: 'str',
+    Any: 'any'
 }
 
 defaults = {
     'int': 0,
-    'str': ''
+    'str': '',
+    'any': None
 }
 
 sizing = {
     'int': lambda num, font: (font.size(str(num))[0], font.size(str(num))[1]+10),
-    'str': lambda txt, font: (font.size(str(txt))[0], font.size(str(txt))[1]+10)
+    'str': lambda txt, font: (font.size(str(txt))[0], font.size(str(txt))[1]+10),
+    'any': lambda _,   font: (font.size(str('None'))[0], font.size(str("None"))[1]+10)
 }
