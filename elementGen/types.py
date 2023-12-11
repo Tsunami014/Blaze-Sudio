a039d05338ffd84cfcc6ecf981bd495901992466
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-12-11 16:05:54 +1100

    feat(nodes): ✨ Made the scrollable have boxes where you set an input! :))))))) THIS WORKS SO WELL!!!
    Made the parsed nodes have values and made boxes in the scrollable that when you change it the value changes and that value gets defaulted with the output

--- elementGen/types.py
+++ elementGen/types.py
@@ -3,7 +3,17 @@ types = {
     'str': str
 }
 
+strtypes = {
+    int: 'int',
+    str: 'str'
+}
+
 defaults = {
     'int': 0,
     'str': ''
+}
+
+sizing = {
+    'int': lambda num, font: (font.size(str(num))[0], font.size(str(num))[1]+10),
+    'str': lambda txt, font: (font.size(str(txt))[0], font.size(str(txt))[1]+10)
 }
