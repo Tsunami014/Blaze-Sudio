ae0ccf8a8ff8479627a272c871a163e591619785
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2023-12-15 07:41:25 +1100

    feat(AIs): ✨ Made the new AI WORK :)))
    fixed a LOT of things

--- utils/bot/BetterModel.py
+++ utils/bot/BetterModel.py
@@ -3,7 +3,7 @@ print('(1/3) Importing... May take a while...') # Takes a long time
 from transformers import pipeline, AutoTokenizer
 print('\033[A(1/3) Importing others...                 ')
 from transformers.generation.streamers import BaseStreamer
-from re import findall
+from re import findall, escape
 
 print('\033[A(2/3) Loading model... May take a while...') # Takes a long time
 pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v0.4")
@@ -11,41 +11,46 @@ print('\033[A(2/3) Loading tokenizer...                            ')
 tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v0.4")
 
 print('\033[A(3/3) Loading other things and finishing up...              ')
+pifg = pipe.model.prepare_inputs_for_generation # A random function with the inputs that are needed for this
 class Streamer(BaseStreamer):
     def __init__(self, default):
         self.default = default.replace('<|im_start|>', '').replace('<|im_end|>', '').strip(' ')
-        for i in findall('[ \t]+?\n[ \t]+?', self.default):
-            self.default = self.default.replace(i, '\n')
-        self.whole = ''
-    def put(self, v):
-        val = tokenizer.batch_decode(v, skip_special_tokens=True)[0]
-        value = val.strip(' ')
-        for i in findall('[ \t]+?\n[ \t]+?', value):
-            value = self.default.replace(i, '\n')
-        if value == self.default:
-            self.whole = ''
+        self.sofar = ''
+        pipe.model.prepare_inputs_for_generation = self.pifg
+    def pifg(self, input_ids, *args, **kwargs):
+        val = tokenizer.batch_decode(input_ids, skip_special_tokens=True)[0]
+        value = val
+        defs = self.default.split('\n')
+        for i in findall('[ \n]*?'.join([escape(i) for i in defs])+'\n', value):
+            value = value.replace(i, '')
+        if value == '':
+            self.sofar = ''
             self.start()
         else:
-            self.whole += val
-            self.output(val, self.whole)
-    def start(self):
-        print('Thinking...')
-    def output(self, val, whole):
-        if whole == val: # It has nothing else in it but the value, so it just started generating
-            print('Generating...')
-            print('Grapefruit: ', end='')
-        print(val, end='')
-    def end(self):
+            self.output(value[len(self.sofar):], value)
+            self.sofar = value
+        return pifg(input_ids, *args, **kwargs)
+    
+    def put(self, v):
+        pass
+    def start(self): # Change this if u want :)
+        print('Thinking...\nGrapefruit: ', end='')
+    def output(self, nexttok, whole): # Change this 2 if u want :D
+        print(nexttok, end='')
+    def end(self): # ANd once again, change this if u want :)
         print('\nFinished!')
 
 CHAT_EOS_TOKEN_ID = 32002
-system_prompt = "Grapefruit is a happy, kind and helpful human assistant who rarely speaks more than a word."
+system_prompt = "Grapefruit is a happy, kind and helpful human assistant who ALWAYS GIVES 2-WORD ANSWERS TO QUESTIONS."
 name = "Grapefruit"
 history = []
 
 print('\033[ADone! Start chatting now:                                 ')
 while True:
-    inp = input('> ')
+    try: inp = input('> ')
+    except:
+        print('Exiting...')
+        break
     history.append(f'<|im_start|>user\n{inp}<|im_end|>')
     history = history[-4:]
     hist = '\n'.join(history)
@@ -53,17 +58,19 @@ while True:
     prompt = (
         f"<|im_start|>system\n{system_prompt}<|im_end|>\n{hist}<|im_start|>{name}\n"
     )
-    res = pipe(prompt, streamer=Streamer(prompt),
-        top_k=50,
-        # top_p = 0.9, do_sample=True # I don't think this is a good idea
-        num_return_sequences=1,
-        repetition_penalty=1.1,
-        max_new_tokens=1024,
-        pad_token_id=CHAT_EOS_TOKEN_ID,
-        eos_token_id=CHAT_EOS_TOKEN_ID
-    )
-    print(res[0]['generated_text'][len(prompt):])
-    history.append(f'<|im_start|>{name}\n{res[0]["generated_text"][len(prompt):]}<|im_end|>')
+    try:
+        res = pipe(prompt, streamer=Streamer(prompt),
+            top_k=50,
+            # top_p = 0.9, do_sample=True # I don't think this is a good idea
+            num_return_sequences=1,
+            repetition_penalty=1.1,
+            max_new_tokens=1024,
+            pad_token_id=CHAT_EOS_TOKEN_ID,
+            eos_token_id=CHAT_EOS_TOKEN_ID
+        )
+        print(res[0]['generated_text'][len(prompt):])
+        history.append(f'<|im_start|>{name}\n{res[0]["generated_text"][len(prompt):]}<|im_end|>')
+    except: print('\nCancelled!')
 
 """
 # Use a pipeline as a high-level helper
