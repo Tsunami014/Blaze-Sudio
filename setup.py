c37f4cdbe1355d1aaf0cafc8e02dfee2dc7ea1b0
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-05 12:22:56 +1100

    fix(build): 🐛 Another attempt at the setup.py configuration
    Attempted a chatGPTed solution and also using setuptools now

--- setup.py
+++ setup.py
@@ -1,31 +1,32 @@
-from distutils.core import setup
-# TODO: finish this
+from setuptools import setup, find_packages
+
 setup(
-  name = 'Blaze-Sudio',         # How you named your package folder
-  packages = ['Blaze-Sudio'],   # Chose the same as "name"
-  version = '1.1.0',      # Start with a small number and increase it with every change you make
-  license='Apache',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'This is the really cool studio to create games!',   # Give a short description about your library
-  author = 'Max Worrall',                   # Type in your name
-  author_email = 'max.worrall@education.nsw.gov.au',      # Type in your E-Mail
-  url = 'https://github.com/Tsunami014/Blaze-Sudio',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Tsunami014/Blaze-Sudio/archive/refs/tags/v1.1.0-alpha.tar.gz',
-  keywords = ['AI', 'Python', 'games'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'requests',
-          'connect-markdown-renderer',
-          'pygame',
-          'gpt4all',
-          'languagemodels',
-          'noise',
-          'scikit-image',
-          'pyLdtk',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    #'Topic :: Software Development :: Build Tools',
-    #'License :: OSI Approved :: MIT License',   # Again, pick a license
-    #'Programming Language :: Python :: 3.10',      # Specify which pyhton versions that you want to support
-  ],
+    name='Blaze-Sudio',
+    packages=find_packages(),
+    data_files=[('', ['*'])],
+    version='1.2.0',
+    license='Apache',
+    description='This is the really cool studio to create games!',
+    author='Max Worrall',
+    author_email='max.worrall@education.nsw.gov.au',
+    url='https://github.com/Tsunami014/Blaze-Sudio',
+    download_url='https://github.com/Tsunami014/Blaze-Sudio/archive/refs/tags/v1.1.0-alpha.tar.gz',
+    keywords=['AI', 'Python', 'games'],
+    install_requires=[
+        'requests',
+        'connect-markdown-renderer',
+        'pygame',
+        'gpt4all',
+        'languagemodels',
+        'noise',
+        'scikit-image',
+        'pyLdtk',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        # 'Topic :: Software Development :: Build Tools',
+        # 'License :: OSI Approved :: MIT License',
+        # 'Programming Language :: Python :: 3.10',
+    ],
 )
