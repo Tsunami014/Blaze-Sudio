ed3fae1dd3463511a31362961b2b5dbf7ebccf3a
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-07 19:11:54 +1100

    feat(build): ✨ Added setup.py for graphics so users can install that separate to the main one

---
+++ graphics/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Blaze-Sudio-Graphics',
+    version='1.3.0',
+    license='Apache',
+    description='This is the graphics portion of https://github.com/Tsunami014/Blaze-Sudio',
+    url='https://github.com/Tsunami014/Blaze-Sudio',
+    long_description_content_type="text/markdown",
+    long_description="For more details see https://github.com/Tsunami014/Blaze-Sudio",
+    packages=find_packages(),
+    author='Max Worrall',
+    author_email='max.worrall@education.nsw.gov.au',
+    install_requires=[
+        'requests',
+        'pygame',
+    ],
+    keywords=['Graphics', 'Python'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        'Topic :: Software Development :: Build Tools',
+        #"Operating System :: Unix",
+        #"Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
+
+# Development Status options: 
+# 1 - Planning
+# 2 - Pre-Alpha
+# 3 - Alpha
+# 4 - Beta
+# 5 - Production/Stable
+# 6 - Mature
+# 7 - Inactive
