1d5f1134eba090bc723a75c8d17bcfc6233d0301
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-11 10:12:43 +1100

    feat(build): ✨ Added back the test for powershell but it just gives a warning this time instead of doing something different

--- graphics/installing/install.bat
+++ graphics/installing/install.bat
@@ -6,10 +6,21 @@ echo Removing unnececary folders for installation
 rmdir "Blaze-Sudio/utils" /S /Q
 rmdir "Blaze-Sudio/elementgen" /S /Q
 
+echo Testing for Powershell...
+setlocal
+CALL :GETPARENT PARENT
+IF /I "%PARENT%" == "powershell" echo Running in powershell, if you see some errors, QUICKLY CLOSE THIS DOWN OR IT WILL INSTALL THE WRONG THING!!!
+endlocal
+
 cd Blaze-Sudio\graphics
 move /Y setup.py ./../setup.py
 cd ..
 pip install .
 cd ../..
 rmdir temp /S /Q
-echo done!
+GOTO :EOF
+
+:GETPARENT
+SET "PSCMD=$ppid=$pid;while($i++ -lt 3 -and ($ppid=(Get-CimInstance Win32_Process -Filter ('ProcessID='+$ppid)).ParentProcessId)) {}; (Get-Process -EA Ignore -ID $ppid).Name"
+
+for /f "tokens=*" %%i in ('powershell -noprofile -command "%PSCMD%"') do SET %1=%%i
