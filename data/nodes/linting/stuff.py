35c737d83c6ca5b62616f147c786006b51d21c4f
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-17 13:14:13 +1100

    feat(nodes): ✨ Completly updated the method of parsing the files, to make them more pythonic

---
+++ data/nodes/linting/stuff.py
@@ -0,0 +1,25 @@
+# Funcs:
+
+def Name(name: str):
+    '''Give this function a name'''
+    pass
+def Docs(doc: str):
+    '''Give this function some documentation'''
+    pass
+
+
+# INFO:
+
+'''
+Use this if you want:
+```
+if type(A) != type(B): raise TypeError('Mismatching types!!!')
+t = type(A)
+```
+'''
+
+'''
+How this works is the dict is the outputs' names to replace by a new value
+So in this case the output with the name 'Out' gets shown as A + B instead 
+of it's original 'Out' value if this doesn't error and has all things connected
+'''
