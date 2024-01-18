844f2be86ff7957b5aedcac2d758b4231682ae95
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-18 19:14:18 +1100

    feat(nodes): ✨ Added a keepname thing :) Now it replaces the name of the thing, that's why I made the keepname

--- data/nodes/math.py
+++ data/nodes/math.py
@@ -15,3 +15,15 @@ def Multiply(A: int, B: int):
 #======#
 def Divide(A: int, B: int):
     return {'Out': A / B}
+#======#
+def Eq(A, B):
+    '''Finds if the two values are equal'''
+    return {'Out': A == B}
+#======#
+def NEq(A, B):
+    '''Finds if the two values are NOT equal'''
+    return {'Out': A == B}
+#======#
+def Not(A: bool):
+    '''Returns the inverse of the input'''
+    return {'Out': not A}
