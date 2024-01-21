1ee6f0a3f0e4285fcd63e6a3dd13237fc1c0fa7e
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-21 13:42:21 +1100

    feat(graphics): ✨ Added Toasts!

--- graphics/GUI/__init__.py
+++ graphics/GUI/__init__.py
@@ -2,3 +2,8 @@ from graphics.GUI.inputbox import RESIZE_W, RESIZE_H, RESIZE_NONE, InputBox, ren
 from graphics.GUI.pyguix import *
 from graphics.GUI.textboxify import *
 from graphics.GUI.randomGUIelements import *
+from graphics.GUI.switch import *
+from graphics.GUI.dropdown import *
+from graphics.GUI.scrollable import *
+from graphics.GUI.colourpick import *
+from graphics.GUI.toast import *
