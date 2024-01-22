9223657c701cc73cfe8e5b001dd649a104902880
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-22 12:11:48 +1100

    feat(main): ✨ Added base files for finding LDtk installation and opening a file with it

---
+++ ldtk/ldtk.py
@@ -0,0 +1,9 @@
+import os
+
+class LDtk:
+    def __init__(self):
+        self.ldtkpath = os.path.join(os.getenv('LOCALAPPDATA'), 'Programs', 'ldtk', 'LDtk.exe')
+        assert os.path.exists(self.ldtkpath), 'LDtk does not exist, or could not be found!' # TODO: make an installation for it
+
+    def open(self, filepath):
+        os.system(self.ldtkpath + ' -c "%s"'%filepath)
