1bab8b7abb7262e590bf2520f6247c2a935a10f6
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-31 09:08:43 +1100

    feat(world): 🚚 Moved ALL stuff to do with LDTK into the ldtk folder

--- ldtk/application.py
+++ ldtk/application.py
@@ -1,6 +1,6 @@
 import os
 
-class LDtk:
+class LDtkAPP:
     def __init__(self):
         self.ldtkpath = os.path.join(os.getenv('LOCALAPPDATA'), 'Programs', 'ldtk', 'LDtk.exe')
         assert os.path.exists(self.ldtkpath), 'LDtk does not exist, or could not be found!' # TODO: make an installation for it
