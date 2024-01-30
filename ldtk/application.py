5a7d03d36f54cae4032583bac942d9bc879a3eec
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-01-31 09:49:01 +1100

    feat(main): ✨ Added base launch function to LDtk app that will never be used... ever

--- ldtk/application.py
+++ ldtk/application.py
@@ -4,6 +4,9 @@ class LDtkAPP:
     def __init__(self):
         self.ldtkpath = os.path.join(os.getenv('LOCALAPPDATA'), 'Programs', 'ldtk', 'LDtk.exe')
         assert os.path.exists(self.ldtkpath), 'LDtk does not exist, or could not be found!' # TODO: make an installation for it
+    
+    def launch(self):
+        os.system(self.ldtkpath)
 
     def open(self, filepath):
         os.system(self.ldtkpath + ' -c "%s"'%filepath)
