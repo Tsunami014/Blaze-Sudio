0553b63b8f7f72614e84f61590fb190c05b19726
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-02-19 08:35:58 +1100

    feat(main): ✨ Added basic movement and character (square)!

--- utils/player.py
+++ utils/player.py
@@ -5,5 +5,16 @@ class Player:
         self.pos = [0, 0]
         self.sur = sur
 
-    def execute(self, win, events):
+    def execute(self, win):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.pos[1] += 10
+        elif keys[pygame.K_DOWN]:
+            self.pos[1] -= 10
+        elif keys[pygame.K_RIGHT]:
+            self.pos[0] -= 10
+        elif keys[pygame.K_LEFT]:
+            self.pos[0] += 10
         win.blit(self.sur, self.pos)
+        mw, mh = win.get_width()/2, win.get_height()/2
+        pygame.draw.rect(win, (0, 0, 0), (mw-20, mh-20, 40, 40), border_radius=2)
