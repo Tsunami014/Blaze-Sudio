a07451156fa7b5091052009f48ab87a95876d9ea
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-02-19 16:23:49 +1100

    feat(main): ✨ Made the ~~little square~~ player move instead of the camera on the edges!

--- utils/player.py
+++ utils/player.py
@@ -7,16 +7,24 @@ class Player:
 
     def execute(self, win):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            self.pos[1] -= 10
-        elif keys[pygame.K_DOWN]:
-            self.pos[1] += 10
-        elif keys[pygame.K_RIGHT]:
-            self.pos[0] += 10
-        elif keys[pygame.K_LEFT]:
-            self.pos[0] -= 10
+        if keys[pygame.K_UP] ^ keys[pygame.K_DOWN]:
+            if keys[pygame.K_UP]:
+                self.pos[1] -= 10
+            elif keys[pygame.K_DOWN]:
+                self.pos[1] += 10
+        if keys[pygame.K_RIGHT] ^ keys[pygame.K_LEFT]:
+            if keys[pygame.K_RIGHT]:
+                self.pos[0] += 10
+            elif keys[pygame.K_LEFT]:
+                self.pos[0] -= 10
         self.pos[0] = max(min(self.pos[0], self.sur.get_width()), 0)
         self.pos[1] = max(min(self.pos[1], self.sur.get_height()), 0)
         mw, mh = win.get_width()/2, win.get_height()/2
-        win.blit(self.sur, [-self.pos[0]+mw, -self.pos[1]+mh])
-        pygame.draw.rect(win, (0, 0, 0), (mw-20, mh-20, 40, 40), border_radius=2)
+        ZC = lambda x: (0 if x < 0 else x) # Zero Check
+        diff = ((ZC(mw-self.pos[0]) or -ZC(self.pos[0]-(self.sur.get_width()-mw))),
+                (ZC(mh-self.pos[1]) or -ZC(self.pos[1]-(self.sur.get_height()-mh))))
+        win.blit(self.sur, [
+            -self.pos[0]+mw-diff[0],
+            -self.pos[1]+mh-diff[1]
+            ])
+        pygame.draw.rect(win, (0, 0, 0), (mw-20-diff[0], mh-20-diff[1], 40, 40), border_radius=2)
