e74a348de7defacbf8e1d5b01e5062129e373491
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-07-03 08:03:48 +1000

    feat(main): 🎉 Started the main Game stuffs!
    RN, it's completly blank.

---
+++ BlazeSudio/Game.py
@@ -0,0 +1,12 @@
+class Game:
+    def __init__(self):
+        pass
+    
+    def load_map(self, fpath):
+        pass
+    
+    def play(self, debug=False): # Play the game
+        pass
+    
+    def build(self): # Launch a screen to show all the things you've defined and what you haven't
+        pass
