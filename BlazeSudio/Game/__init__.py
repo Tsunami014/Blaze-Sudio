05cd9eed691a44e333cac63eb94d376c4fd7926c
Tsunami014 <max.worrall@education.nsw.gov.au>
2024-07-25 21:03:08 +1000

    fix(main): 🐛 Fixed 2 issues with the help screen and also added the ability to easily add more settings without it being an utter pain!

--- BlazeSudio/Game/__init__.py
+++ BlazeSudio/Game/__init__.py
@@ -1,23 +1,34 @@
 from BlazeSudio.graphics import Graphic
 from BlazeSudio.graphics import options as GO
 from BlazeSudio.utils import Player
-import BlazeSudio.Game.world as world
 from pyperclip import copy
 
+import BlazeSudio.Game.world as world
+import BlazeSudio.Game.statics as statics
+
+
 __all__ = [
+    "Game",
     "world",
-    "Game"
+    "statics"
 ]
 
 G = Graphic()
+
+_types = {
+    "scale": statics.Number,
+    "gravity": statics.Iterable
+}
 
 class Game:
     def __init__(self):
         self.world = None
         self.debug = None
         self.settings = {
-            "scale": 1
+            "scale": 1,
+            "gravity": [0, -1]
         }
+        self._onticks = []
     
     def load_map(self, fpath):
         self.world = world.World(fpath)
@@ -32,6 +43,17 @@ class Game:
             The scale of the map
         """
         self.settings.update(kwargs)
+    
+    def ontick(self, func):
+        """
+        Add a function to be called every tick
+        
+        Parameters
+        ----------
+        func : function
+            The function to be called
+        """
+        self._onticks.append(func)
     
     @G.CGraphic
     def play(self, event, element=None, aborted=False, debug=False):
@@ -59,22 +81,28 @@ class Game:
                     List all the commands!
                     """
                     if event == GO.ELOADUI:
-                        G.add_text('ALL COMMANDS', GO.CBLACK, GO.PCTOP, GO.FTITLE)
+                        G.Clear()
                         G.add_text('Press "esc" to go back', GO.CBLACK, GO.PCTOP)
                         newG, e = G.add_Scrollable(GO.PLCENTER, (G.size[0]/3, G.size[0]/2), (G.size[0]/3, G.size[0]))
                         newG2, e = G.add_Scrollable(GO.PRCENTER, (G.size[0]/3, G.size[0]/2), (G.size[0]/3, G.size[0]))
                         rainbow = GO.CRAINBOW()
+                        newG.add_empty_space(GO.PCTOP, 0, 10)
+                        newG.add_text('ALL COMMANDS', GO.CBLACK, GO.PCTOP, GO.FTITLE)
                         def add_cmd(cmd, txt, cpy):
                             col = next(rainbow)
-                            newG.add_text(cmd, col, GO.PCTOP)
-                            newG.add_text(txt, col, GO.PCTOP)
+                            newG.add_text(cmd, col, GO.PCTOP, allowed_width=G.size[0]/3-10)
+                            newG.add_text(txt, col, GO.PCTOP, allowed_width=G.size[0]/3-10)
                             newG.add_button('Copy command', col, GO.PCTOP, callback=lambda e: copy(cpy))
                         add_cmd('/help ...', 'Get help; a list of all the commands you can use!', '/help')
                         add_cmd('/items ...', 'Go to the items page: to view all the entities and other useful information in the game/level!', '/items')
                         add_cmd('/set <setting> <setTo>', 'Set a setting. See "Settings"', '/set ')
                         add_cmd('/get <setting> ...', 'Gets a setting. See "Settings"', '/get ')
+                        newG2.add_empty_space(GO.PCTOP, 0, 10)
                         newG2.add_text('Settings', GO.CBLACK, GO.PCTOP, GO.FTITLE)
-                        newG2.add_text('"scale": the scale of the level', GO.CBLACK, GO.PCTOP)
+                        def add_sett(txt):
+                            newG2.add_text(txt, GO.CBLACK, GO.PCTOP, allowed_width=G.size[0]/3-10)
+                        add_sett('"scale" (number): the scale of the level')
+                        add_sett('"gravity" (tuple[int,int]): the gravity of the player, x and y')
                 
                 @G.Graphic
                 def items(event, element=None, aborted=False):
@@ -97,16 +125,18 @@ class Game:
                         if len(args) != 3:
                             G.Toast('Incorrect amount of arguments for command "/set"! Expected: 2')
                         else:
-                            if args[1] == 'scale':
-                                if not args[2].isnumeric():
-                                    G.Toast('Expected argument 2 of "/set" to be a number, but was not!')
-                                else:
-                                    self.settings['scale'] = int(args[2])
+                            if args[1] in self.settings:
+                                try:
+                                    out = eval(args[1])
+                                    assert isinstance(out, _types[args[1]])
+                                    self.settings[args[1]] = out
+                                except:
+                                    G.Toast(f'Expected argument 2 of "/set" to be a {str(_types[args[1]])}, but was not!')
                             else:
                                 G.Toast('Invalid setting to set!')
                     elif args[0] == '/get':
-                        if args[1] == 'scale':
-                            G.Toast(f"Scale is {self.settings['scale']}")
+                        if args[1] in self.settings:
+                            G.Toast(f"{args[1]} is {self.settings[args[1]]}")
                         else:
                             G.Toast('Invalid setting to get!')
                     elif args[0] == '/help':
@@ -115,3 +145,6 @@ class Game:
                         items()
                     else:
                         G.Toast('Invalid command! for help use /help')
+        elif event == GO.ETICK:
+            for i in self._onticks:
+                i()
