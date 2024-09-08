edb0d27f813c1343a0379a61d8e0e58a7d3865f7
Tsunami014 (Max) <96847801+Tsunami014@users.noreply.github.com>
2024-09-08 14:17:27 +1000

    feat(physics): ✨ Added back in all the collisions and now it's running at like 2 FPS but still running :)

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ G.SetSettings(scale=8, gravity=[0, 0.1])
 @G.Collision
 class Collisions(Ss.BaseCollisions):
     def __call__(self, pos, accel, entity):
-        ret = collisions.Rect(pos[0]-0.5, pos[1]-0.5, 1, 1).handleCollisionsAccel(accel, collisions.Shapes(*G.currentLvL.layers[1].intgrid.getRects(1)[0:2]), False)
+        ret = collisions.Rect(pos[0]-0.5, pos[1]-0.5, 1, 1).handleCollisionsAccel(accel, G.currentLvL.layers[1].intgrid.getRects(1), False)
         return (ret[0][0][0]+0.5, ret[0][0][1]+0.5), ret[1]
 
 @G.Player
