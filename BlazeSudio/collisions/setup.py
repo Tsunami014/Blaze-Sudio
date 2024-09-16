ddc2c47987aa0eb516838f8c6fb782c66c663e84
Tsunami014 (Max) <96847801+Tsunami014@users.noreply.github.com>
2024-09-16 20:33:13 +1000

    fix(build): 🐛 Fixed an incorrect file location

--- BlazeSudio/collisions/setup.py
+++ BlazeSudio/collisions/setup.py
@@ -10,7 +10,7 @@ extensions = [
 
 # Build pyi stub file automatically
 os.system("stubgen lib/collisions.py --include-docstrings")
-shutil.copy('./out/collisions/lib/collisions.pyi', './collisions.pyi')
+shutil.copy('./out/BlazeSudio/collisions/lib/collisions.pyi', './collisions.pyi')
 
 if '--no-build' not in sys.argv:
     setup(
