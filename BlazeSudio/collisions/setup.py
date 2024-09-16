828dac200d3d209e9cddfc97cd646c0da4adb1a5
Tsunami014 (Max) <96847801+Tsunami014@users.noreply.github.com>
2024-09-17 08:09:25 +1000

    fix(physics): 🐛 turns out renaming the built files breaks everything

--- BlazeSudio/collisions/setup.py
+++ BlazeSudio/collisions/setup.py
@@ -10,17 +10,8 @@ extensions = [
 os.system("stubgen lib/collisions.py --include-docstrings")
 shutil.copy('./out/BlazeSudio/collisions/lib/collisions.pyi', './collisions.pyi')
 
-oldfs = os.listdir('./')
-
 if '--no-build' not in sys.argv:
     setup(
         name="collisions",
         ext_modules=cythonize(extensions),
     )
-
-newfs = [i for i in os.listdir('./') if i not in oldfs]
-for f in newfs:
-    newname = re.sub('collisions\\.cpython-\\d+-', 'collisions-', f)
-    if os.path.exists(newname):
-        os.remove(newname)
-    os.rename(f, newname)
