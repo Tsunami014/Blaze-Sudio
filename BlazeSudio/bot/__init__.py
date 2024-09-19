1337e3f5f5bad053a285ee5c306d755d634ce15c
Tsunami014 (Max) <Tsunami014@duck.com>
2024-09-19 13:51:42 +1000

    chore(other): 🔥 Removed all things not Blaze Sudio related for the new release

--- BlazeSudio/bot/__init__.py
+++
@@ -1,7 +0,0 @@
-from BlazeSudio.bot.gpt4real import *
-from BlazeSudio.bot.set_preferences import *
-from BlazeSudio.bot.AIs import *
-from BlazeSudio.bot.basebots import *
-from BlazeSudio.bot.install_tinyllm import *
-from BlazeSudio.bot.tinyllm import *
-#from bot.copilot import * # errors...
--- BlazeSudio/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from BlazeSudio.utils.characters import *
-from BlazeSudio.utils.discussions import *
-from utils.converse import *
