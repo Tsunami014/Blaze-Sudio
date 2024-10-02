ac6490433b0f932b4367009d0a0c2c4a64f41e58
Tsunami014 (Max) <Tsunami014@duck.com>
2024-10-02 17:49:38 +1000

    fix(graphics): 💥 Fixed a bug with no tileset in the get image and also re-did the wrapping function so you have to implement a lot of it yourself for more control

--- BlazeSudio/utils/__init__.py
+++ BlazeSudio/utils/__init__.py
@@ -1,3 +1,3 @@
-from BlazeSudio.utils.wrap import wrapWorld
+from BlazeSudio.utils.wrap import wrapLevel
 from BlazeSudio.utils.wrap import save as saveWrapped
 from BlazeSudio.utils.genCollisions import approximate_polygon
