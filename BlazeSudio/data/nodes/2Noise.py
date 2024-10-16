8defa94492affea8e344a96f03e3632658933eff
Tsunami014 (Max) <Tsunami014@duck.com>
2024-10-16 17:14:20 +1100

    feat(nodes): ✨ Added some params to the noise func and fixed one or two bugs

--- BlazeSudio/data/nodes/2Noise.py
+++ BlazeSudio/data/nodes/2Noise.py
@@ -1,17 +1,26 @@
 from BlazeSudio.elementGen import Image as _Image
 from noise import snoise2 as _snoise2
 
-def PerlinNoise():
+def PerlinNoise(octaves, xScale, yScale):
     """PerlinNoise
     Make some noise!
+
+    Args:
+        octaves (int): The number of octaves to use for the noise. Defaults to 6.
+        xScale (number): The scale of the noise in the x direction. Defaults to 10.
+        yScale (number): The scale of the noise in the y direction. Defaults to 10.
 
     Returns:
         Img (Image): The generated noise!
     """
-    octaves = 6
+    octaves = int(octaves)
+    if octaves <= 0:
+        octaves = 1
+    xScale = max(float(xScale), 0.1)
+    yScale = max(float(yScale), 0.1)
     imgSze = (100, 100)
     def getNoise(x, y):
-        n = int((_snoise2(x / 10, y / 10, octaves)+1) * (255/2))
+        n = int((_snoise2(x / xScale, y / yScale, octaves)+1) * (255/2))
         return (n, n, n)
     imgDat = [
         [
