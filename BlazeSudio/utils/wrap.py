71bef4258c26689b7a2b1b41a2e7fd514875cb07
Tsunami014 (Max) <Tsunami014@duck.com>
2024-10-27 09:10:46 +1100

    fix(other): 🐛 Fixed a transparency issue

--- BlazeSudio/utils/wrap.py
+++ BlazeSudio/utils/wrap.py
@@ -68,7 +68,7 @@ def save(imgs, fname, szes):
     prevh = 0
     for img, sze in zip(imgs, szes):
         if isinstance(img, pygame.Surface):
-            newImg = pygame.transform.scale(img, (sze, sze)).convert()
+            newImg = pygame.transform.scale(img, (sze, sze)).convert_alpha()
             out.blit(newImg, (0, prevh))
             prevh += newImg.get_height()
             continue
