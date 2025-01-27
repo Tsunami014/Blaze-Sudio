785ef7bab9608ae77db0734390d3ec7d4df53839
Tsunami014 (Max) <Tsunami014@duck.com>
2025-01-28 10:22:59 +1100

    feat(main): ✨ Added some funky gravity shenanigans for the BaseEntity physics

--- BlazeSudio/Game/statics.py
+++ BlazeSudio/Game/statics.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 # Only import game if is typing
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -92,8 +93,12 @@ class BaseEntity(SkeletonEntity):
         # Things to change in __init__ and most of the time remain the same:
         self.max_speed = 3
         """Max speed"""
+        self.max_grav_speed = 3
+        """Max speed the gravity can get you"""
         self.friction = 0.1
-        """Friction (applied each frame) (in percent of current speed)"""
+        """Friction perpendicular to gravity direction (or if no gravity) (applied each frame) (in percent of current speed)"""
+        self.grav_fric = 0.1
+        """Friction applied in gravity direction (each frame) (in percent of current gravity strength)"""
         self.not_hold_fric = 0.01
         """ADDED friction to apply when not holding ANY KEY (you can modify this to be only left-right or whatever) (in percent of current speed)"""
         self.not_hold_grav = [0, 0.04]
@@ -137,17 +142,33 @@ class BaseEntity(SkeletonEntity):
     def apply_physics(self):
         # Cap the target speed to max_speed
         self.velocity = [max(-self.max_speed, min(self.velocity[0], self.max_speed)),
-                                max(-self.max_speed, min(self.velocity[1], self.max_speed))]
+                         max(-self.max_speed, min(self.velocity[1], self.max_speed))]
 
-        # Apply deceleration to target, so over time (if not constantly set) will slow down
-        self.velocity = [self.velocity[0] * (1-(self.friction+(self.not_hold_fric if not self.holding_any else 0))),
-                                self.velocity[1] * (1-(self.friction+(self.not_hold_fric if not self.holding_any else 0)))]
+        fric = 1 - (self.friction + (self.not_hold_fric if not self.holding_any else 0))
 
-        # Add gravity
-        self.velocity = [
-            self.velocity[0]+(self.gravity[0]*(1 if not self.holding_jmp else (1-self.not_hold_grav[0]))),
-            self.velocity[1]+(self.gravity[1]*(1 if not self.holding_jmp else (1-self.not_hold_grav[1])))
-        ]
+        # Project velocity onto gravity direction
+        g_mag = math.hypot(*self.gravity)
+        if g_mag != 0:
+            gdir = (self.gravity[0]/g_mag, self.gravity[1]/g_mag)
+            vdot = self.velocity[0]*gdir[0] + self.velocity[1]*gdir[1]
+            vdot_fric = max(0, vdot - (self.grav_fric + (self.not_hold_fric if not self.holding_any else 0))*g_mag)
+            parallel = (vdot_fric*gdir[0], vdot_fric*gdir[1])
+            perpendicular = ((self.velocity[0]-parallel[0])*fric, 
+                             (self.velocity[1]-parallel[1])*fric)
+
+            self.velocity = [parallel[0]+perpendicular[0],
+                             parallel[1]+perpendicular[1]]
+        else:
+            self.velocity = [self.velocity[0]*fric, 
+                             self.velocity[1]*fric]
+        
+        # Apply gravity if under max grav speed
+        def sign(x):
+            return 1 if x > 0 else -1 if x < 0 else 0
+        if sign(self.velocity[0]) != sign(self.gravity[0]) or abs(self.velocity[0]) < self.max_grav_speed:
+            self.velocity[0] += self.gravity[0]*(1 if not self.holding_jmp else (1-self.not_hold_grav[0]))
+        if sign(self.velocity[1]) != sign(self.gravity[1]) or abs(self.velocity[1]) < self.max_grav_speed:
+            self.velocity[1] += self.gravity[1]*(1 if not self.holding_jmp else (1-self.not_hold_grav[1]))
 
         # Cap the speed to max_speed
         self.velocity = [max(-self.max_speed, min(self.velocity[0], self.max_speed)),
