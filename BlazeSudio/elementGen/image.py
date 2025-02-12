d36d42f9ed23a075b9ef8d642b5b9c1fe3cebe7f
Tsunami014 (Max) <Tsunami014@duck.com>
2025-02-12 20:31:10 +1100

    fix(nodes): 🚑️ Made the node editor work again!
    With a DIY perlin noise func too!

--- BlazeSudio/elementGen/image.py
+++ BlazeSudio/elementGen/image.py
@@ -6,7 +6,7 @@ GLOBALSZE = [100, 100]
 class Image:
     def __init__(self, data=None):
         if data is None:
-            self.data = [[(0, 0, 0)]]
+            self.data = []
         else:
             self.data = data
     
@@ -15,13 +15,50 @@ class Image:
         return GLOBALSZE
     
     def _toBuffer(self):
-        data = []
-        for i in range(GLOBALSZE[1]):
-            for j in range(GLOBALSZE[0]):
-                data.extend(self.get(i, j))
-        return bytes(data)
+        map = self.getMap(0, 0, *GLOBALSZE)
+        return bytes([k for i in map for j in i for k in j])
     
-    def get(self, y, x):
+    def __getitem__(self, it):
+        if not isinstance(it, tuple):
+            raise ValueError(
+                'Image indices must be a tuple!'
+            )
+        outit = []
+        for i in it:
+            if isinstance(it, slice):
+                outit.append([i.start, i.stop, i.step])
+            elif isinstance(it, int):
+                outit.append([i, i])
+            else:
+                raise ValueError(
+                    'Image indices must be slices or ints!'
+                )
+        if len(it) != 2:
+            raise ValueError(
+                'Image indices must be 2D!'
+            )
+        
+        if any(len(i) != 2 for i in it):
+            li = self.getMap(it[0][0], it[1][0], it[0][1], it[1][1])
+            xstep = 1
+            ystep = 1
+            if it[0][2] is not None:
+                xstep = it[0][2]
+            if it[1][2] is not None:
+                ystep = it[1][2]
+            return [
+                li[y][::ystep] for y in range(0, len(li), xstep)
+            ]
+        return self.get(it[0][0], it[1][0])
+    
+    def getMap(self, x, y, xTo, yTo):
+        return [
+            [self.get(x2, y2) for x2 in range(x, xTo)] for y2 in range(y, yTo)
+        ]
+
+    def get(self, x, y):
+        if len(self.data) == 0:
+            return (0, 0, 0)
         return self.data[y % len(self.data)][x % len(self.data[0])]
 
     def to_pygame(self):
--- BlazeSudio/elementGen/nodes/3Noise.py
+++ BlazeSudio/elementGen/nodes/3Noise.py
@@ -1,13 +1,81 @@
-from BlazeSudio.elementGen import Image as _Image
-from noise import snoise2 as _snoise2
-from random import randint as _randint
+from BlazeSudio.elementGen import Image
+import random
+import numpy as np
+
+__all__ = [
+    'PerlinNoise'
+]
+
+# Thanks so much to https://pvigier.github.io/2018/06/13/perlin-noise-numpy.html for the awesome perlin noise code!
+def generate_perlin_noise_2d(shape, res):
+    def f(t):
+        return 6*t**5 - 15*t**4 + 10*t**3
+
+    delta = (res[0] / shape[0], res[1] / shape[1])
+    d = (shape[0] // res[0], shape[1] // res[1])
+    grid = np.mgrid[0:res[0]:delta[0],0:res[1]:delta[1]].transpose(1, 2, 0) % 1
+    # Gradients
+    angles = 2*np.pi*np.random.rand(res[0]+1, res[1]+1)
+    gradients = np.dstack((np.cos(angles), np.sin(angles)))
+    g00 = gradients[0:-1,0:-1].repeat(d[0], 0).repeat(d[1], 1)
+    g10 = gradients[1:,0:-1].repeat(d[0], 0).repeat(d[1], 1)
+    g01 = gradients[0:-1,1:].repeat(d[0], 0).repeat(d[1], 1)
+    g11 = gradients[1:,1:].repeat(d[0], 0).repeat(d[1], 1)
+    # Ramps
+    n00 = np.sum(grid * g00, 2)
+    n10 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1])) * g10, 2)
+    n01 = np.sum(np.dstack((grid[:,:,0], grid[:,:,1]-1)) * g01, 2)
+    n11 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]-1)) * g11, 2)
+    # Interpolation
+    t = f(grid)
+    n0 = n00*(1-t[:,:,0]) + t[:,:,0]*n10
+    n1 = n01*(1-t[:,:,0]) + t[:,:,0]*n11
+    return np.sqrt(2)*((1-t[:,:,1])*n0 + t[:,:,1]*n1)
+
+def generate_fractal_noise_2d(shape, res, octaves=1, persistence=0.5):
+    noise = np.zeros(shape)
+    frequency = 1
+    amplitude = 1
+    for _ in range(octaves):
+        noise += amplitude * generate_perlin_noise_2d(shape, (frequency*res[0], frequency*res[1]))
+        frequency *= 2
+        amplitude *= persistence
+    return noise
+
+class NoisyImage(Image):
+    def __init__(self, data=None, /, octaves=1, seed=0, scale=(10, 10)):
+        self.octaves, self.seed = octaves, seed
+        self.xScale, self.yScale = scale
+        self.cache = {}
+        super().__init__(data)
+    
+    def get(self, x, y):
+        return self.getMap(x, y, x+1, y+1)[0][0]
+    
+    def getMap(self, x, y, xTo, yTo):
+        cacheSearch = (x, y, xTo, yTo)
+        if cacheSearch in self.cache:
+            return self.cache[cacheSearch]
+        w, h = int(xTo - x), int(yTo - y)
+        np.random.seed(self.seed)
+        res = (max(1, w // 4), max(1, h // 4))
+        noise_img = generate_fractal_noise_2d((w, h), res, self.octaves)
+        
+        end = [[(val, val, val) for i in j for val in (int((i + 1) * 127.5),)] for j in noise_img]
+
+        if len(self.cache) > 127:
+            self.cache.pop(list(self.cache.keys())[0])
+
+        self.cache[cacheSearch] = end
+
+        return end
 
 def PerlinNoise(octaves, xScale, yScale, seed):
     """PerlinNoise
     Make some noise!
 
     Args:
-        octaves (int): The number of octaves to use for the noise. Defaults to 6.
+        octaves (int): The number of octaves to use for the noise. Defaults to 1.
         xScale (number): The scale of the noise in the x direction. Defaults to 10.
         yScale (number): The scale of the noise in the y direction. Defaults to 10.
         seed (int): The seed for the noise, 0 = random. Defaults to 0.
@@ -22,10 +90,6 @@ def PerlinNoise(octaves, xScale, yScale, seed):
     yScale = max(float(yScale), 0.1)
     base = float(seed)
     if base == 0:
-        base = _randint(-999999, 999999)
-    def getNoise(x, y):
-        n = int((_snoise2(x / xScale, y / yScale, octaves, base=base)+1) * (255/2))
-        return (n, n, n)
-    img = _Image()
-    img.get = getNoise
-    return img
+        base = random.randint(0, 4294967295) # 2**32-1
+    
+    return NoisyImage(octaves=octaves, seed=base, scale=(xScale, yScale))
