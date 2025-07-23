ef1ce48c0240405d6c968c1f93e6188f4ba18618
Tsunami014 <tsunami014@duck.com>
2025-07-23 16:23:46 +1000

    Improved the speedup code a lot!

--- BlazeSudio/debug/collisions.py
+++ BlazeSudio/debug/collisions.py
@@ -1,3 +1,8 @@
+"""
+This uses the not compiled version of the collisions library; useful for debugging.
+
+Use as `from BlazeSudio.debug import collisions` as a drop-in replacement for `from BlazeSudio import collisions`
+"""
 import os as _os
 _os.environ['debugCollisions'] = 'True'
 from BlazeSudio.collisions import *
