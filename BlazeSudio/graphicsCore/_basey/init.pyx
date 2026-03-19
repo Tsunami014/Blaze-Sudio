3df94cdaab9f734feb98613734ef605891ed0ca8
Tsunami014 <tsunami014@duck.com>
2026-03-19 15:56:00 +1100

    Sped up the _basey class a little

--- BlazeSudio/graphicsCore/_basey/init.pyx
+++ BlazeSudio/graphicsCore/_basey/init.pyx
@@ -1,6 +1,9 @@
 # cython: boundscheck=False, wraparound=False, cdivision=True, initializedcheck=False
 import numpy as np
-class Base:
+cimport numpy as cnp
+__cimport_types__ = [cnp.ndarray]
+
+cdef class Base:
     def _warpPs(self, mat: np.ndarray, points: np.ndarray):
         points = points.astype(float)
         if self._affMat(mat):
@@ -8,35 +11,42 @@ class Base:
         p = np.c_[points, np.ones(len(points))] @ mat.T
         return p[:, :2] / p[:, 2:3]
 
-    def _regMat(self, mat: np.ndarray):
+    cpdef bint _regMat(self,
+            cnp.ndarray[cnp.float64_t, ndim=2] mat):
         """Returns True if a matrix is 'regular' - no rotations or perspective warps"""
+        cdef double[:, ::1] mat_mv = mat
         return (
-            mat[2, 0] == 0 and mat[2, 1] == 0 and mat[2, 2] == 1 and
+            mat_mv[2, 0] == 0 and mat_mv[2, 1] == 0 and mat_mv[2, 2] == 1 and
             (
-                (mat[0,1] == 0 and mat[1,0] == 0) or
-                (mat[0,1] == 0 and mat[0,1] == [0, 0])
+                (mat_mv[0,1] == 0 and mat_mv[1,0] == 0) or
+                (mat_mv[0,0] == 0 and mat_mv[1,1] == 0)
             )
         )
-    def _affMat(self, mat: np.ndarray):
+    cpdef bint _affMat(self,
+            cnp.ndarray[cnp.float64_t, ndim=2] mat):
         """Returns True if a matrix is 'affine' - no perspective warps"""
-        return mat[2, 0] == 0 and mat[2, 1] == 0 and mat[2, 2] == 1
+        cdef double[:, ::1] mat_mv = mat
+        return mat_mv[2, 0] == 0 and mat_mv[2, 1] == 0 and mat_mv[2, 2] == 1
 
-    def _regWarp(self, mat: np.ndarray, p, offset: bool = True):
+    cpdef cnp.ndarray[cnp.float64_t, ndim=2] _regWarp(self,
+            cnp.ndarray[cnp.float64_t, ndim=2] mat, object p, bint offset = True):
         """Warps a point assuming no perspective warp (bottom row is [0, 0, 1])"""
-        cdef float x = p[0]
-        cdef float y = p[1]
+        cdef double[:, ::1] mat_mv = mat
+        cdef double x = p[0]
+        cdef double y = p[1]
         if offset:
             return np.array([
-                x*mat[0, 0] + y*mat[0, 1] + mat[0, 2],
-                x*mat[1, 0] + y*mat[1, 1] + mat[1, 2]
+                x*mat_mv[0, 0] + y*mat_mv[0, 1] + mat_mv[0, 2],
+                x*mat_mv[1, 0] + y*mat_mv[1, 1] + mat_mv[1, 2]
             ], dtype=float)
         else:
             return np.array([
-                x*mat[0, 0] + y*mat[0, 1],
-                x*mat[1, 0] + y*mat[1, 1]
+                x*mat_mv[0, 0] + y*mat_mv[0, 1],
+                x*mat_mv[1, 0] + y*mat_mv[1, 1]
             ], dtype=float)
 
-    def _warpbbx(self, mat, crop, outercrop):
+    cpdef object _warpbbx(self,
+            cnp.ndarray[cnp.float64_t, ndim=2] mat, object crop, object outercrop):
         topL = crop[:2]
         botR = crop[2:]
         if self._regMat(mat):
@@ -51,20 +61,29 @@ class Base:
                 botR,
                 [botR[0], topL[1]]
             ], float))
-        tl = (min(p[0] for p in ps),
-              min(p[1] for p in ps))
-        br = (max(p[0] for p in ps),
-              max(p[1] for p in ps))
+        cdef left = ps[0][0]
+        cdef top = ps[0][1]
+        cdef right = ps[0][0]
+        cdef bot = ps[0][1]
+        for p in ps[1:]:
+            if p[0] < left:
+                left = p[0]
+            if p[0] > right:
+                right = p[0]
+            if p[1] < top:
+                top = p[1]
+            if p[1] > bot:
+                bot = p[1]
         if outercrop[2] != 0:
-            tl = (
-                max(outercrop[0], tl[0]),
-                max(outercrop[1], tl[1]),
-            )
-            br = (
-                min(outercrop[2], br[0]),
-                min(outercrop[3], br[1]),
-            )
-        if tl[0] >= br[0] or tl[1] >= br[1]:
-            return (0, 0, 0, 0)
-        return (tl[0], tl[1], br[0], br[1])
+            if outercrop[0] > left:
+                left = outercrop[0]
+            if outercrop[2] < right:
+                right = outercrop[2]
+            if outercrop[1] > top:
+                top = outercrop[1]
+            if outercrop[3] < bot:
+                bot = outercrop[3]
+        if left >= right or top >= bot:
+            return (0,0,0,0)
+        return (left, top, right, bot)
 
